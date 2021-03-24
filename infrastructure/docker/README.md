79632e9f29f289418ea4f5eba6ae9f9c14f383ad
Jens Kutzsche <github@gebea.de>
2021-03-24 15:41:59 +0100

    #34 Docker compose extended with SORMAS and cleaned up + variable usage
    Sets default port in dummy-app to this from dockerized public server.
    Fixes NPE if parameter -f not set for dummy-app.

---
+++ infrastructure/docker/README.md
@@ -0,0 +1,4 @@
+# Docker
+
+All docker related setup does here. The `postgres` we are using needs some additional users and setup. 
+It is based on SORMAS-Docker [postgres](https://github.com/hzi-braunschweig/SORMAS-Docker/tree/master/postgres).
--- infrastructure/docker/postgres/pg-init-scripts/create-multiple-postgresql-databases.sh
+++ infrastructure/docker/postgres/pg-init-scripts/create-multiple-postgresql-databases.sh
@@ -13,7 +13,7 @@ function create_user_and_database() {
 EOSQL
 }
 
-if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+if [ -v POSTGRES_MULTIPLE_DATABASES ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
 		create_user_and_database $db
