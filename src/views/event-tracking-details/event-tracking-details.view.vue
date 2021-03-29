b36108ebebe11ee1297ae11109bbc6a1c456c843
Johannes Kreutz <jkm@jkm-marburg.de>
2021-03-29 19:10:31 +0200

    Add event tracking details view

--- src/views/event-tracking-details/event-tracking-details.view.vue
+++ src/views/event-tracking-details/event-tracking-details.view.vue
@@ -1,7 +1,46 @@
 <template>
   <div>
-    <h1>Details für Ereignis {{ eventId }}</h1>
-    <p>TODO</p>
+    <v-card>
+      <v-card-title>Details für Ereignis {{ eventId }}</v-card-title>
+      <v-card-text>
+        <v-col cols="8">
+          <v-row>
+            Name:
+            {{ eventData.name }}
+          </v-row>
+          <v-row>
+            Ort:
+            {{ eventData.address }}
+          </v-row>
+          <v-row>
+            Zeitraum:
+            {{ eventData.startTime }} - {{ eventData.endTime }}
+          </v-row>
+        </v-col>
+        <v-text-field
+          v-model="tableData.search"
+          append-icon="mdi-magnify"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+        <v-data-table
+          :headers="tableData.headers"
+          :items="tableData.eventParticipants"
+          :items-per-page="5"
+          class="elevation-1 mt-5"
+          :search="tableData.search"
+        >
+
+        </v-data-table>
+        <v-row class="mt-2">
+          <v-col cols="12">
+            <v-btn class="ml-2 mr-2" color="white" @click="on">Zurück </v-btn>
+            <v-btn class="mr-2 float-right" color="primary" @click="on">Auswahl exportieren </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
@@ -12,5 +51,68 @@ import { Component, Vue } from "vue-property-decorator";
 @Component
 export default class EventTrackingDetailsView extends Vue {
   eventId = router.currentRoute.params.id;
+  eventData = {
+    extID: "GTOAZEIC",
+    name: "Toms Bierbrunnen",
+    address: "Nobistor 14, 22767 Hamburg",
+    startTime: "30.03.2021 17:00",
+    endTime: "30.03.2021 22:00",
+    generatedTime: "31.03.2021 09:44",
+    status: "Angefragt",
+    lastChange: "31.03.2021 09:44",
+  };
+  tableData = {
+    search: "",
+    headers: [
+      {
+        text: "ID",
+        align: "start",
+        sortable: true,
+        value: "id",
+      },
+      {
+        text: "Nachname",
+        value: "lastname",
+      },
+      {
+        text: "Vorname",
+        value: "firstname",
+      },
+      {
+        text: "Check-In",
+        value: "checkInTime",
+      },
+      {
+        text: "Check-Out",
+        value: "checkOutTime",
+      },
+      {
+        text: "Kommentar",
+        value: "comment",
+      }
+    ],
+    eventParticipants: [
+      {
+        id: "74UJ89",
+        lastname: "Auerbach",
+        firstname: "Dan",
+        checkInTime: "26.03.2021 12:11",
+        checkOutTime: "26.03.2021 12:57",
+        comment: "An der Bar mit 2 weiteren Personen",
+      },
+      {
+        id: "4TTS31",
+        lastname: "Irvine",
+        firstname: "Weldon",
+        checkInTime: "26.03.2021 12:12",
+        checkOutTime: "26.03.2021 13:57",
+        comment: "Tisch 7",
+      }
+    ],
+  };
+
+  on(): void {
+    console.log("NOT IMPLEMENTED");
+  }
 }
 </script>
