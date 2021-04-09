8d89229607d93854387d3011bb8191250d3f4fd9
andreas.kausler <aka@cosynus.de>
2021-04-09 15:01:57 +0200

    add lodash & types - for flattenDeep functionality added a comment to explain why we set the container max-width for medium screen sizes to none

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,9 +1,4 @@
-export const flattenDeep = (arr: Array<unknown>): Array<unknown> => {
-  return arr.reduce<unknown[]>((acc, val) => {
-    const value = Array.isArray(val) ? flattenDeep(val) : val;
-    return acc.concat(value);
-  }, []);
-};
+import { flattenDeep } from "lodash";
 
 export const join = (arr: Array<unknown>, separator: string): string => {
   return flattenDeep(arr)
