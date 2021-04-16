0bfb60752dbb8cecf7a75fdf1c1833b76ca53907
Nuts <mad-nuts@users.noreply.github.com>
2021-04-16 11:44:04 +0200

    #27 dashboard improvements (#44)
    - remove charts
    - change wording
    - reset vuex store on route leave
    - add error handler + message

--- src/views/home/home.store.ts
+++ src/views/home/home.store.ts
@@ -6,10 +6,12 @@ import { clientConfig } from "@/main";
 import { RootState } from "@/store/types";
 
 import { Commit, Module } from "vuex";
+import { ErrorMessage, getErrorMessage } from "@/utils/axios";
 
 export type HomeState = {
   eventTrackingList: ExistingDataRequestClientWithLocationList | null;
   eventTrackingListLoading: boolean;
+  eventTrackingListError: ErrorMessage;
 };
 
 export interface HomeModule extends Module<HomeState, RootState> {
@@ -19,19 +21,24 @@ export interface HomeModule extends Module<HomeState, RootState> {
       eventTrackingList: ExistingDataRequestClientWithLocationList | null
     ): void;
     setEventTrackingListLoading(state: HomeState, payload: boolean): void;
+    setEventTrackingListError(state: HomeState, payload: ErrorMessage): void;
+    reset(state: HomeState, payload: null): void;
   };
   actions: {
     fetchEventTrackingList({ commit }: { commit: Commit }): Promise<void>;
   };
 }
 
+const defaultState: HomeState = {
+  eventTrackingList: null,
+  eventTrackingListLoading: false,
+  eventTrackingListError: null,
+};
+
 const home: HomeModule = {
   namespaced: true,
   state() {
-    return {
-      eventTrackingList: null,
-      eventTrackingListLoading: false,
-    };
+    return { ...defaultState };
   },
   mutations: {
     setEventTrackingList(state, eventTrackingList) {
@@ -40,15 +47,25 @@ const home: HomeModule = {
     setEventTrackingListLoading(state, loading) {
       state.eventTrackingListLoading = loading;
     },
+    setEventTrackingListError(state, error: ErrorMessage) {
+      state.eventTrackingListError = error;
+    },
+    reset(state) {
+      // we can keep the data, no need to reset it
+      state.eventTrackingListLoading = false;
+    },
   },
   actions: {
     async fetchEventTrackingList({ commit }) {
       const client = IrisClientFrontendApiFactory(clientConfig);
       let eventTrackingList: ExistingDataRequestClientWithLocationList | null = null;
+      commit("setEventTrackingListError", null);
       commit("setEventTrackingListLoading", true);
       try {
         eventTrackingList = (await client.dataRequestsClientLocationsGet())
           .data;
+      } catch (e) {
+        commit("setEventTrackingListError", getErrorMessage(e));
       } finally {
         commit("setEventTrackingList", eventTrackingList);
         commit("setEventTrackingListLoading", false);
