2712be396e4512b769db7d8055d0ace7492b9561
Jens Kutzsche <github@gebea.de>
2021-04-20 21:06:57 +0200

    iris-gateway/iris-backlog#33 fixes bug with non set properties

--- iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/auth/db/jwt/JwtProperties.java
+++ iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/auth/db/jwt/JwtProperties.java
@@ -1,17 +1,20 @@
 package de.healthIMIS.iris.client.auth.db.jwt;
 
-import lombok.*;
+import lombok.Data;
+import lombok.NonNull;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Data
+@ConstructorBinding
 @ConfigurationProperties(prefix = "security.jwt")
 @ConditionalOnProperty(
-        value="security.auth",
-        havingValue = "db"
-)
+		value = "security.auth",
+		havingValue = "db")
 public class JwtProperties {
 
-  private @NonNull String jwtSharedSecret;
+	private @NonNull String jwtSharedSecret;
 
 }
