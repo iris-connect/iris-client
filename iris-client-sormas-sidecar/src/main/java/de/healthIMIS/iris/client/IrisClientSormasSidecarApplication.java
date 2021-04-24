48f6bd5b16aad4c02e8fe6637c630968510690d3
Jens Kutzsche <github@gebea.de>
2021-04-24 09:26:11 +0200

    Output the version and commit of Spring applications to the console.

--- iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/IrisClientSormasSidecarApplication.java
+++ iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/IrisClientSormasSidecarApplication.java
@@ -14,9 +14,12 @@
  *******************************************************************************/
 package de.healthIMIS.iris.client;
 
+import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -26,7 +29,20 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ConfigurationPropertiesScan
 public class IrisClientSormasSidecarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IrisClientSormasSidecarApplication.class, args);
+	public static void main(String[] args) throws Exception {
+
+		var properties = PropertiesLoaderUtils.loadAllProperties("git.properties");
+		var banner = new ResourceBanner(new ClassPathResource("iris-banner.txt")) {
+			@Override
+			protected String getApplicationVersion(Class<?> sourceClass) {
+				return properties.getProperty("git.build.version", "-") + " ("
+						+ properties.getProperty("git.commit.id.abbrev", "-")
+						+ ")";
+			}
+		};
+
+		var application = new SpringApplication(IrisClientSormasSidecarApplication.class);
+		application.setBanner(banner);
+		application.run(args);
 	}
 }
