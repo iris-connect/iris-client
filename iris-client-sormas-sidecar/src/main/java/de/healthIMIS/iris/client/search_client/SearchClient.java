c6dbbbdb9a0fe4a64335f66b6370b4d4337a95f8
Jens Kutzsche <github@gebea.de>
2021-04-24 09:35:45 +0200

    iris-gateway/iris-backlog#69 renames properties class to follow convent.

--- iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/search_client/SearchClient.java
+++ iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/search_client/SearchClient.java
@@ -5,6 +5,7 @@ import de.healthIMIS.iris.api.sidecarclient.model.LocationList;
 import de.healthIMIS.iris.client.data_request.Location;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -14,29 +15,26 @@ import org.springframework.web.client.RestTemplate;
 @AllArgsConstructor
 public class SearchClient {
 
-    private SearchClientConfiguration config;
+	private SearchClientProperties config;
 
-    @Qualifier("search-rest")
-    private RestTemplate rest;
+	@Qualifier("search-rest")
+	private RestTemplate rest;
 
-    private final @NonNull ModelMapper mapper;
+	private final @NonNull ModelMapper mapper;
 
-    public Location findByProviderIdAndLocationId(String providerId, String locationId) {
-        var locationDto = rest.getForObject(config.getEndpoint()+ "/{providerId}/{locationId}",
-                LocationInformation.class,
-                providerId, locationId);
-        var location = mapper.map(locationDto, Location.class);
-        location.setId(new Location.LocationIdentifier());
-        location.setLocationId(locationDto.getId());
-        // Todo: Why is it null
-        location.setProviderId(providerId);
-        return location;
-    }
+	public Location findByProviderIdAndLocationId(String providerId, String locationId) {
+		var locationDto = rest.getForObject(config.getEndpoint() + "/{providerId}/{locationId}",
+				LocationInformation.class,
+				providerId, locationId);
+		var location = mapper.map(locationDto, Location.class);
+		location.setId(new Location.LocationIdentifier());
+		location.setLocationId(locationDto.getId());
+		// Todo: Why is it null
+		location.setProviderId(providerId);
+		return location;
+	}
 
-    public LocationList search(String keyword) {
-        var locationListDto = rest.getForObject(config.getEndpoint()+ "/{keyword}",
-            LocationList.class, keyword);
-        return locationListDto;
-    }
-
+	public LocationList search(String keyword) {
+		return rest.getForObject(config.getEndpoint() + "/{keyword}", LocationList.class, keyword);
+	}
 }
--- iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/search_client/SearchClientProperties.java
+++ iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/search_client/SearchClientProperties.java
@@ -1,17 +1,18 @@
 package de.healthIMIS.iris.client.search_client;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@ConstructorBinding
 @ConfigurationProperties(prefix = "iris.location-service")
 @Getter
-@Setter
-@NoArgsConstructor
-public class SearchClientConfiguration {
+@AllArgsConstructor
+public class SearchClientProperties {
 
     private String endpoint;
 
