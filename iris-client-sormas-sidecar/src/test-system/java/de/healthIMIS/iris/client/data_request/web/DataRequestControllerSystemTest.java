269571de06b82ba2da8a936d8a42ae1893248fa1
Jens Kutzsche <github@gebea.de>
2021-04-24 09:41:03 +0200

    iris-gateway/iris-backlog#69 adds test and fix ignored constrain
    The length constraint for search keyword on location search had no
    effect so far. This is fixed now and the keyword must have a length of 4
    otherwise a 400 is returned.

--- iris-client-sormas-sidecar/src/test-system/java/de/healthIMIS/iris/client/data_request/web/DataRequestControllerSystemTest.java
+++ iris-client-sormas-sidecar/src/test-system/java/de/healthIMIS/iris/client/data_request/web/DataRequestControllerSystemTest.java
@@ -1,104 +1,101 @@
 package de.healthIMIS.iris.client.data_request.web;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+
+import de.healthIMIS.iris.client.IrisWebIntegrationTest;
 import de.healthIMIS.iris.client.data_request.DataRequest;
 import de.healthIMIS.iris.client.data_request.DataRequestManagement;
 import de.healthIMIS.iris.client.data_request.Location;
 import de.healthIMIS.iris.client.data_request.web.dto.ExistingDataRequestClientWithLocationList;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.mockito.internal.util.collections.Sets;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.internal.util.collections.Sets;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@ActiveProfiles("dev")
-@SpringBootTest
-@AutoConfigureMockMvc
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@IrisWebIntegrationTest
 class DataRequestControllerSystemTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+	@Autowired
+	private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper om;
+	@Autowired
+	private ObjectMapper om;
 
+	@MockBean
+	private DataRequestManagement dataRequestManagement;
 
-    @MockBean
-    private DataRequestManagement dataRequestManagement;
+	@Test
+	public void endpointShouldBeProtected() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/data-requests-client/locations"))
+				.andExpect(MockMvcResultMatchers.status().isForbidden()).andReturn();
+	}
 
-    @Test
-    public void endpointShouldBeProtected() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/data-requests-client/locations")
-        ).andExpect(MockMvcResultMatchers.status().isForbidden()).andReturn();
-    }
+	@Test
+	@WithMockUser()
+	public void getDataRequests() throws Exception {
+		var res = mockMvc.perform(
+				MockMvcRequestBuilders
+						.get("/data-requests-client/locations"))
+				.andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
 
-    @Test
-    @WithMockUser()
-    public void getDataRequests() throws Exception {
-        var res = mockMvc.perform(
-                MockMvcRequestBuilders
-                        .get("/data-requests-client/locations")
-        ).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+		var dataRequests = om.readValue(res.getResponse().getContentAsString(),
+				ExistingDataRequestClientWithLocationList.class);
 
-        var dataRequests = om.readValue(res.getResponse().getContentAsString(), ExistingDataRequestClientWithLocationList.class);
+		assertEquals(0, dataRequests.getDataRequests().size());
 
-        assertEquals(0, dataRequests.getDataRequests().size());
+	}
 
-    }
+	@Test
+	@WithMockUser()
+	public void getDataRequestByCode() throws Exception {
+		postNewDataRequest();
 
-    @Test
-    @WithMockUser()
-    public void getDataRequestByCode() throws Exception {
-        postNewDataRequest();
+		mockMvc.perform(
+				MockMvcRequestBuilders
+						.get("/data-requests-client/locations/123"))
+				.andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+	}
 
-        mockMvc.perform(
-                MockMvcRequestBuilders
-                        .get("/data-requests-client/locations/123")
-        ).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
-    }
+	@Test
+	@WithMockUser()
+	public void createDataRequest() throws Exception {
+		postNewDataRequest();
+	}
 
-    @Test
-    @WithMockUser()
-    public void createDataRequest() throws Exception {
-        postNewDataRequest();
-    }
+	private void postNewDataRequest() throws Exception {
+		var dataRequest = Mockito.spy(new DataRequest("refId", "name", ZonedDateTime.now().toInstant(),
+				ZonedDateTime.now().toInstant(), "requestDetails",
+				"", new Location(), Sets.newSet(DataRequest.Feature.Guests)));
 
-    private void postNewDataRequest() throws Exception {
-        var dataRequest = Mockito.spy(new DataRequest("refId", "name", ZonedDateTime.now().toInstant(), ZonedDateTime.now().toInstant(), "requestDetails",
-                "", new Location(), Sets.newSet(DataRequest.Feature.Guests)));
+		// set by JPA on live calls, needs to be mocked here
+		Mockito.doReturn(LocalDateTime.now()).when(dataRequest).getCreatedAt();
+		Mockito.doReturn(LocalDateTime.now()).when(dataRequest).getLastModifiedAt();
 
-        // set by JPA on live calls, needs to be mocked here
-        Mockito.doReturn(LocalDateTime.now()).when(dataRequest).getCreatedAt();
-        Mockito.doReturn(LocalDateTime.now()).when(dataRequest).getLastModifiedAt();
+		Mockito.when(
+				dataRequestManagement.createLocationRequest(any(), any(), any(), any(), any(), any(), any()))
+				.thenReturn(dataRequest);
 
-        Mockito.when(
-                dataRequestManagement.createLocationRequest(any(), any(), any(), any(), any(), any(), any())
-        ).thenReturn(dataRequest);
+		Mockito.when(
+				dataRequestManagement.findById(anyString())).thenReturn(Optional.of(dataRequest));
 
-        Mockito.when(
-                dataRequestManagement.findById(anyString())
-        ).thenReturn(Optional.of(dataRequest));
-
-        mockMvc.perform(
-                MockMvcRequestBuilders.post("/data-requests-client/locations")
-                        .content(TestData.DATA_REQUEST)
-                        .contentType(MediaType.APPLICATION_JSON)
-        ).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
-    }
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/data-requests-client/locations")
+						.content(TestData.DATA_REQUEST)
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
+	}
 }
