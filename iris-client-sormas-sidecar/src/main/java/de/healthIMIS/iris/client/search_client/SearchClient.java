d3e5761b208067c1b7416889ee73adf51711a8ac
SWlionmint <43147540+SWlionmint@users.noreply.github.com>
2021-04-28 09:38:51 +0200

    Feature/search client tests (#81)
    I have written the tests for the search client.
    
    Changes were made to findByProviderIdAndLocationId by adding null checks
    
    The Todo should not be necessary anymore. The mapper has been tested and works.
    No origin for the initial problem could be found in the iris-location-service. getLocation with providerId and locationId has no tests yet, but the mapper in toDto is the same as in search(String keyword) and as such should work.
    The original error might already have been removed.

--- iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/search_client/SearchClient.java
+++ iris-client-sormas-sidecar/src/main/java/de/healthIMIS/iris/client/search_client/SearchClient.java
@@ -28,9 +28,23 @@ public class SearchClient {
 				providerId, locationId);
 		var location = mapper.map(locationDto, Location.class);
 		location.setId(new Location.LocationIdentifier());
-		location.setLocationId(locationDto.getId());
-		// Todo: Why is it null
-		location.setProviderId(providerId);
+		
+		if(location.getLocationId()==null) 
+		{
+			if(locationDto.getId()!=null) {
+				location.setLocationId(locationDto.getId());
+			} else {
+				location.setLocationId(locationId);				
+			}
+		}
+		
+		if(location.getProviderId()==null) {
+			if(locationDto.getProviderId()!=null) {
+				location.setProviderId(locationDto.getProviderId());
+			} else {
+				location.setProviderId(providerId);				
+			}
+		}
 		return location;
 	}
 
