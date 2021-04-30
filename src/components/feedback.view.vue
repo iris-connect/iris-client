71ffde0790af9f33cadf92ae3bd89c6ee0a3ce8e
Fabian Szönyi <f.szoenyi@ostfalia.de>
2021-04-30 14:04:29 +0200

    added confirm and cancel dialog

--- src/components/feedback.view.vue
+++ src/components/feedback.view.vue
@@ -91,14 +91,70 @@
 
         <v-row>
           <v-col class="d-flex justify-center">
-            <v-btn color="blue darken-1" text outlined @click="show = false">
+            <v-btn
+              color="blue darken-1"
+              text
+              outlined
+              @click="showCancelDialog = true"
+            >
               Abbrechen
             </v-btn>
           </v-col>
           <v-col class="d-flex justify-center">
-            <v-btn color="green accent-2" outlined text> Absenden </v-btn>
+            <v-btn
+              color="green accent-2"
+              outlined
+              text
+              @click="showConfirmDialog = true"
+            >
+              Absenden
+            </v-btn>
           </v-col>
         </v-row>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog
+      style="overflow-y: hidden"
+      v-model="showCancelDialog"
+      max-width="20%"
+    >
+      <v-card>
+        <v-card-title>
+          <span>Abbrechen</span>
+          <v-spacer></v-spacer>
+          Sind sie sich Sicher, dass sie den Feedback Bogen schliesen wollen.\n
+          Das Schliesen über den Abbrechen Knopf löscht alle geschriebenen
+          Daten.
+        </v-card-title>
+        <v-card-actions>
+          <v-btn color="primary" text @click="showCancelDialog = false">
+            Abbrechen
+          </v-btn>
+          <v-btn color="primary" text @click="cancel()"> Schliesen </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog
+      style="overflow-y: hidden"
+      v-model="showConfirmDialog"
+      max-width="20%"
+    >
+      <v-card>
+        <v-card-title>
+          <span>Bestätigen</span>
+          <v-spacer></v-spacer>
+          Sind sie sich Sicher, dass sie den Feedback Bogen abschicken
+          wollen.Die Daten werden in einem GitHub Issue gespeichert und es wird
+          ihnen ein Link zum Issue zu ihrer E-Mail Addresse gesendet.
+        </v-card-title>
+        <v-card-actions>
+          <v-btn color="primary" text @click="showConfirmDialog = false">
+            Abbrechen
+          </v-btn>
+          <v-btn color="primary" text @click="confirm()"> Abschicken </v-btn>
+        </v-card-actions>
       </v-card>
     </v-dialog>
   </v-row>
@@ -106,6 +162,22 @@
 
 <script>
 export default {
+  data() {
+    return {
+      showCancelDialog: false,
+      showConfirmDialog: false,
+    };
+  },
+  methods: {
+    cancel() {
+      this.showCancelDialog = false;
+      this.show = false;
+    },
+    confirm() {
+      this.showConfirmDialog = false;
+      this.show = false;
+    },
+  },
   props: {
     value: Boolean,
   },
