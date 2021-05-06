8137e27d112c797a486218cdba6a36f5d832c9d3
Tim <62595633+lucky-lusa@users.noreply.github.com>
2021-05-06 16:49:18 +0200

    LocationSearchController uses EPS to query LocationService

--- iris-client-bff/src/main/java/iris/client_bff/search_client/web/LocationSearchController.java
+++ iris-client-bff/src/main/java/iris/client_bff/search_client/web/LocationSearchController.java
@@ -1,11 +1,14 @@
 package iris.client_bff.search_client.web;
 
+import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
+import iris.client_bff.config.RPCClientConfig;
 import iris.client_bff.search_client.SearchClient;
 import iris.client_bff.search_client.web.dto.LocationList;
+import iris.client_bff.search_client.web.dto.SearchRequestDto;
 import lombok.RequiredArgsConstructor;
+import javax.net.ssl.SSLContext;
 
-import javax.validation.constraints.Size;
-
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -14,18 +17,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.net.URL;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
+import java.util.HashMap;
+
 @RestController
 @Validated
 @RequiredArgsConstructor
 public class LocationSearchController {
 
-	private final SearchClient searchClient;
+    private final SearchClient searchClient;
 
-	@GetMapping("/search/{search_keyword}")
-	@ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<LocationList> searchSearchKeywordGet(
-			@Size(min = 4) @PathVariable("search_keyword") String searchKeyword) {
+    private final @NotNull JsonRpcHttpClient rpcClient;
 
-		return new ResponseEntity<>(searchClient.search(searchKeyword), HttpStatus.OK);
-	}
+    @GetMapping("/search/{search_keyword}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<LocationList> searchSearchKeywordGet(
+            @Size(min = 4) @PathVariable("search_keyword") String searchKeyword) throws Throwable {
+
+        SearchRequestDto search = SearchRequestDto.builder().searchKeyword(searchKeyword).build();
+
+        LocationList locations = rpcClient.invoke("ls-1.searchForLocation", search, LocationList.class);
+
+        return new ResponseEntity<>(locations, HttpStatus.OK);
+    }
 }
