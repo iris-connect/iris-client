942c9c6977b5ba1c0b0dcf6a989c3b089b9b8140
Claudio Bianucci <claudio.bianucci@lionmint.com>
2021-05-07 18:54:33 +0200

    Fix unit tests

--- iris-client-bff/src/test/java/iris/client_bff/search_client/eps/EPSSearchClientTests.java
+++ iris-client-bff/src/test/java/iris/client_bff/search_client/eps/EPSSearchClientTests.java
@@ -1,9 +1,19 @@
 package iris.client_bff.search_client.eps;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.matches;
+import static org.mockito.Mockito.when;
+
+import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
 import iris.client_bff.IrisWireMockTest;
 import iris.client_bff.search_client.dto.LocationInformation;
 import iris.client_bff.search_client.dto.LocationList;
+import iris.client_bff.search_client.eps.dto.IdSearch;
+import iris.client_bff.search_client.eps.dto.KeywordSearch;
 import iris.client_bff.search_client.exceptions.IRISSearchException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 
 import java.io.IOException;
@@ -11,7 +21,9 @@ import java.io.InputStream;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 
@@ -32,83 +44,75 @@ public class EPSSearchClientTests {
 	@Autowired
 	private ResourceLoader resourceLoader;
 
+	@MockBean
+	private JsonRpcHttpClient rpcClient;
+
 	@Test
-	public void searchByKeywordReturnsLocation() throws IOException, IRISSearchException {
+	public void searchByKeywordReturnsLocation() throws Throwable {
+
 		// given
-		String searchPath = SEARCH_PATH + SEARCH_KEY;
+		var mockedSearchResult = new LocationList().locations(List.of(new LocationInformation()
+				.id(LOCATION_ID)
+				.name(NAME)
+				.providerId(PROVIDER_ID)));
 
-		Resource resource = resourceLoader
-				.getResource("classpath:testdata/SearchClientSearchByKeywordReturnsLocation.json");
-		InputStream jsonStream = resource.getInputStream();
-		String json = new String(jsonStream.readAllBytes());
+		var payload = KeywordSearch.builder().searchKeyword(SEARCH_KEY).build();
 
-		var locItem = new LocationInformation().id(LOCATION_ID).name("name").providerId(PROVIDER_ID);
-		var locList = new LocationList().addLocationsItem(locItem);
+		when(rpcClient.invoke(matches(".*\\.searchForLocation"), eq(payload), eq(LocationList.class)))
+				.thenReturn(mockedSearchResult);
 
-		WireMock.stubFor(WireMock.get(WireMock.urlEqualTo(searchPath))
-				.willReturn(
-						WireMock.aResponse().withStatus(200).withHeader("Content-Type", "application/json").withBody(json)));
-
-		// do
-		LocationList searchResults = systemUnderTest.search(SEARCH_KEY);
+		// when
+		LocationList actualSearchResults = systemUnderTest.search(SEARCH_KEY);
 
 		// then
-		Assertions.assertNotNull(searchResults);
-		Assertions.assertNotNull(searchResults.getLocations());
-		Assertions.assertEquals(searchResults.getLocations().size(), locList.getLocations().size());
-		Assertions.assertNotNull(searchResults.getLocations().get(0));
-		Assertions.assertEquals(searchResults.getLocations().get(0).getId(), locItem.getId());
-		Assertions.assertEquals(searchResults.getLocations().get(0).getName(), locItem.getName());
-		Assertions.assertEquals(searchResults.getLocations().get(0).getProviderId(), locItem.getProviderId());
+		assertEquals(mockedSearchResult, actualSearchResults);
 	}
 
 	@Test
-	public void searchByIdReturnsLocation() throws IOException, IRISSearchException {
+	public void findByProviderIdAndLocationId() throws Throwable {
+
 		// given
-		String searchPath = SEARCH_PATH + PROVIDER_ID + "/" + LOCATION_ID;
-		Resource resource = resourceLoader.getResource("classpath:testdata/SearchClientSearchByIdReturnsLocation.json");
-		InputStream jsonStream = resource.getInputStream();
-		String json = new String(jsonStream.readAllBytes());
+		var mockedSearchResult = new LocationInformation()
+				.id(LOCATION_ID)
+				.name(NAME)
+				.providerId(PROVIDER_ID);
 
-		var locItem = new LocationInformation().id(LOCATION_ID).name(NAME).providerId(PROVIDER_ID);
+		var payload = IdSearch.builder().locationId(LOCATION_ID).providerId(PROVIDER_ID).build();
 
-		WireMock.stubFor(WireMock.get(WireMock.urlEqualTo(searchPath))
-				.willReturn(
-						WireMock.aResponse().withStatus(200).withHeader("Content-Type", "application/json").withBody(json)));
+		when(rpcClient.invoke(matches(".*\\.getLocationDetails"), eq(payload), eq(LocationInformation.class)))
+				.thenReturn(mockedSearchResult);
 
-		// do
-		var searchLocation = systemUnderTest.findByProviderIdAndLocationId(PROVIDER_ID, LOCATION_ID);
+		// when
+		var actualSearchResults = systemUnderTest.findByProviderIdAndLocationId(PROVIDER_ID, LOCATION_ID);
 
 		// then
-		Assertions.assertNotNull(searchLocation);
-		Assertions.assertEquals(searchLocation.getId(), locItem.getId());
-		Assertions.assertEquals(searchLocation.getProviderId(), locItem.getProviderId());
-		Assertions.assertEquals(searchLocation.getName(), locItem.getName());
+		assertEquals(mockedSearchResult, actualSearchResults);
 	}
 
 	@Test
-	public void searchByIdReturnsLocationWithoutLocationIdAndProviderId()
-			throws IOException, IRISSearchException {
-		// given
-		String searchPath = SEARCH_PATH + PROVIDER_ID + "/" + LOCATION_ID;
-		Resource resource = resourceLoader
-				.getResource("classpath:testdata/SearchClientSearchByIdReturnsLocationWithoutLocationIdAndProviderId.json");
-		InputStream jsonStream = resource.getInputStream();
-		String json = new String(jsonStream.readAllBytes());
+	public void findByProviderIdAndLocationId_augmentedIds() throws Throwable {
 
-		var locItem = new LocationInformation().id(LOCATION_ID).name(NAME).providerId(PROVIDER_ID);
+		// given search response without provider id and location id
+		var mockedSearchResult = new LocationInformation()
+				.id(null)
+				.name(NAME)
+				.providerId(null);
 
-		WireMock.stubFor(WireMock.get(WireMock.urlEqualTo(searchPath))
-				.willReturn(
-						WireMock.aResponse().withStatus(200).withHeader("Content-Type", "application/json").withBody(json)));
+		var payload = IdSearch.builder().locationId(LOCATION_ID).providerId(PROVIDER_ID).build();
 
-		// do
-		var searchLocation = systemUnderTest.findByProviderIdAndLocationId(PROVIDER_ID, LOCATION_ID);
+		when(rpcClient.invoke(matches(".*\\.getLocationDetails"), eq(payload), eq(LocationInformation.class)))
+				.thenReturn(mockedSearchResult);
 
-		// then
-		Assertions.assertNotNull(searchLocation);
-		Assertions.assertEquals(searchLocation.getId(), locItem.getId());
-		Assertions.assertEquals(searchLocation.getProviderId(), locItem.getProviderId());
-		Assertions.assertEquals(searchLocation.getName(), locItem.getName());
+		// when
+		var actualSearchResults = systemUnderTest.findByProviderIdAndLocationId(PROVIDER_ID, LOCATION_ID);
+
+		// then provider id and location id which were given in request are found in result
+
+		var mockedSearchResultWithIds = new LocationInformation()
+				.id(LOCATION_ID)
+				.name(NAME)
+				.providerId(PROVIDER_ID);
+
+		assertEquals(mockedSearchResultWithIds, actualSearchResults);
 	}
 }
