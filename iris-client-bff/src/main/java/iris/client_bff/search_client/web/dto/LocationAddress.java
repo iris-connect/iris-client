860086c4e94fa114528e161bb5720616955ff5b8
Jens Kutzsche <github@gebea.de>
2021-05-11 08:57:24 +0200

    chore: removes swagger-annotations dependency and all uses of these ann.

--- iris-client-bff/src/main/java/iris/client_bff/search_client/web/dto/LocationAddress.java
+++ iris-client-bff/src/main/java/iris/client_bff/search_client/web/dto/LocationAddress.java
@@ -12,145 +12,134 @@
 
 package iris.client_bff.search_client.web.dto;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.annotation.JsonTypeName;
 
 /**
  * Anschrift des Standorts
  */
-@ApiModel(description = "Anschrift des Standorts")
 @JsonPropertyOrder({
-		LocationAddress.JSON_PROPERTY_STREET,
-		LocationAddress.JSON_PROPERTY_CITY,
-		LocationAddress.JSON_PROPERTY_ZIP
+	LocationAddress.JSON_PROPERTY_STREET,
+	LocationAddress.JSON_PROPERTY_CITY,
+	LocationAddress.JSON_PROPERTY_ZIP
 })
-@JsonTypeName("LocationAddress")
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen",
-		date = "2021-05-03T15:54:40.838481+02:00[Europe/Berlin]")
 public class LocationAddress {
-	public static final String JSON_PROPERTY_STREET = "street";
-	private String street;
+  public static final String JSON_PROPERTY_STREET = "street";
+  private String street;
 
-	public static final String JSON_PROPERTY_CITY = "city";
-	private String city;
+  public static final String JSON_PROPERTY_CITY = "city";
+  private String city;
 
-	public static final String JSON_PROPERTY_ZIP = "zip";
-	private String zip;
+  public static final String JSON_PROPERTY_ZIP = "zip";
+  private String zip;
 
-	public LocationAddress street(String street) {
+  public LocationAddress street(String street) {
 
-		this.street = street;
-		return this;
+	this.street = street;
+	return this;
+  }
+
+  /**
+   * street + number
+   * 
+   * @return street
+   **/
+  @JsonProperty(JSON_PROPERTY_STREET)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+
+  public String getStreet() {
+	return street;
+  }
+
+  public void setStreet(String street) {
+	this.street = street;
+  }
+
+  public LocationAddress city(String city) {
+
+	this.city = city;
+	return this;
+  }
+
+  /**
+   * Stadt
+   * 
+   * @return city
+   **/
+  @JsonProperty(JSON_PROPERTY_CITY)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+
+  public String getCity() {
+	return city;
+  }
+
+  public void setCity(String city) {
+	this.city = city;
+  }
+
+  public LocationAddress zip(String zip) {
+
+	this.zip = zip;
+	return this;
+  }
+
+  /**
+   * Postleitzahl
+   * 
+   * @return zip
+   **/
+  @JsonProperty(JSON_PROPERTY_ZIP)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+
+  public String getZip() {
+	return zip;
+  }
+
+  public void setZip(String zip) {
+	this.zip = zip;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+	if (this == o) {
+	  return true;
 	}
-
-	/**
-	 * street + number
-	 * 
-	 * @return street
-	 **/
-	@ApiModelProperty(example = "Türkenstr. 7", required = true, value = "street + number")
-	@JsonProperty(JSON_PROPERTY_STREET)
-	@JsonInclude(value = JsonInclude.Include.ALWAYS)
-
-	public String getStreet() {
-		return street;
+	if (o == null || getClass() != o.getClass()) {
+	  return false;
 	}
+	LocationAddress locationAddress = (LocationAddress) o;
+	return Objects.equals(this.street, locationAddress.street) &&
+		Objects.equals(this.city, locationAddress.city) &&
+		Objects.equals(this.zip, locationAddress.zip);
+  }
 
-	public void setStreet(String street) {
-		this.street = street;
+  @Override
+  public int hashCode() {
+	return Objects.hash(street, city, zip);
+  }
+
+  @Override
+  public String toString() {
+	StringBuilder sb = new StringBuilder();
+	sb.append("class LocationAddress {\n");
+	sb.append("    street: ").append(toIndentedString(street)).append("\n");
+	sb.append("    city: ").append(toIndentedString(city)).append("\n");
+	sb.append("    zip: ").append(toIndentedString(zip)).append("\n");
+	sb.append("}");
+	return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+	if (o == null) {
+	  return "null";
 	}
-
-	public LocationAddress city(String city) {
-
-		this.city = city;
-		return this;
-	}
-
-	/**
-	 * Stadt
-	 * 
-	 * @return city
-	 **/
-	@ApiModelProperty(example = "München", required = true, value = "Stadt")
-	@JsonProperty(JSON_PROPERTY_CITY)
-	@JsonInclude(value = JsonInclude.Include.ALWAYS)
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public LocationAddress zip(String zip) {
-
-		this.zip = zip;
-		return this;
-	}
-
-	/**
-	 * Postleitzahl
-	 * 
-	 * @return zip
-	 **/
-	@ApiModelProperty(example = "80333", required = true, value = "Postleitzahl")
-	@JsonProperty(JSON_PROPERTY_ZIP)
-	@JsonInclude(value = JsonInclude.Include.ALWAYS)
-
-	public String getZip() {
-		return zip;
-	}
-
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		LocationAddress locationAddress = (LocationAddress) o;
-		return Objects.equals(this.street, locationAddress.street) &&
-				Objects.equals(this.city, locationAddress.city) &&
-				Objects.equals(this.zip, locationAddress.zip);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(street, city, zip);
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("class LocationAddress {\n");
-		sb.append("    street: ").append(toIndentedString(street)).append("\n");
-		sb.append("    city: ").append(toIndentedString(city)).append("\n");
-		sb.append("    zip: ").append(toIndentedString(zip)).append("\n");
-		sb.append("}");
-		return sb.toString();
-	}
-
-	/**
-	 * Convert the given object to string with each line indented by 4 spaces (except the first line).
-	 */
-	private String toIndentedString(Object o) {
-		if (o == null) {
-			return "null";
-		}
-		return o.toString().replace("\n", "\n    ");
-	}
+	return o.toString().replace("\n", "\n    ");
+  }
 
 }
--- iris-client-bff/src/main/java/iris/client_bff/search_client/web/dto/LocationList.java
+++ iris-client-bff/src/main/java/iris/client_bff/search_client/web/dto/LocationList.java
@@ -12,8 +12,6 @@
 
 package iris.client_bff.search_client.web.dto;
 
-import io.swagger.annotations.ApiModelProperty;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -21,83 +19,75 @@ import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.annotation.JsonTypeName;
 
-/**
- * LocationList
- */
 @JsonPropertyOrder({
-		LocationList.JSON_PROPERTY_LOCATIONS
+	LocationList.JSON_PROPERTY_LOCATIONS
 })
-@JsonTypeName("LocationList")
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen",
-		date = "2021-05-03T15:54:40.838481+02:00[Europe/Berlin]")
 public class LocationList {
-	public static final String JSON_PROPERTY_LOCATIONS = "locations";
-	private List<LocationInformation> locations = new ArrayList<>();
+  public static final String JSON_PROPERTY_LOCATIONS = "locations";
+  private List<LocationInformation> locations = new ArrayList<>();
 
-	public LocationList locations(List<LocationInformation> locations) {
+  public LocationList locations(List<LocationInformation> locations) {
 
-		this.locations = locations;
-		return this;
+	this.locations = locations;
+	return this;
+  }
+
+  public LocationList addLocationsItem(LocationInformation locationsItem) {
+	this.locations.add(locationsItem);
+	return this;
+  }
+
+  /**
+   * Get locations
+   * 
+   * @return locations
+   **/
+  @JsonProperty(JSON_PROPERTY_LOCATIONS)
+  @JsonInclude(value = JsonInclude.Include.ALWAYS)
+
+  public List<LocationInformation> getLocations() {
+	return locations;
+  }
+
+  public void setLocations(List<LocationInformation> locations) {
+	this.locations = locations;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+	if (this == o) {
+	  return true;
 	}
-
-	public LocationList addLocationsItem(LocationInformation locationsItem) {
-		this.locations.add(locationsItem);
-		return this;
+	if (o == null || getClass() != o.getClass()) {
+	  return false;
 	}
+	LocationList locationList = (LocationList) o;
+	return Objects.equals(this.locations, locationList.locations);
+  }
 
-	/**
-	 * Get locations
-	 * 
-	 * @return locations
-	 **/
-	@ApiModelProperty(required = true, value = "")
-	@JsonProperty(JSON_PROPERTY_LOCATIONS)
-	@JsonInclude(value = JsonInclude.Include.ALWAYS)
+  @Override
+  public int hashCode() {
+	return Objects.hash(locations);
+  }
 
-	public List<LocationInformation> getLocations() {
-		return locations;
+  @Override
+  public String toString() {
+	StringBuilder sb = new StringBuilder();
+	sb.append("class LocationList {\n");
+	sb.append("    locations: ").append(toIndentedString(locations)).append("\n");
+	sb.append("}");
+	return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+	if (o == null) {
+	  return "null";
 	}
-
-	public void setLocations(List<LocationInformation> locations) {
-		this.locations = locations;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if (o == null || getClass() != o.getClass()) {
-			return false;
-		}
-		LocationList locationList = (LocationList) o;
-		return Objects.equals(this.locations, locationList.locations);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(locations);
-	}
-
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append("class LocationList {\n");
-		sb.append("    locations: ").append(toIndentedString(locations)).append("\n");
-		sb.append("}");
-		return sb.toString();
-	}
-
-	/**
-	 * Convert the given object to string with each line indented by 4 spaces (except the first line).
-	 */
-	private String toIndentedString(Object o) {
-		if (o == null) {
-			return "null";
-		}
-		return o.toString().replace("\n", "\n    ");
-	}
+	return o.toString().replace("\n", "\n    ");
+  }
 
 }
