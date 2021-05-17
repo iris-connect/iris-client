ba37edd6b1673d298d67dda283285193200395d9
Tim <62595633+lucky-lusa@users.noreply.github.com>
2021-05-17 09:10:33 +0200

    Added support for _client Object form EPS

--- iris-client-bff/src/main/java/iris/client_bff/events/eps/DataSubmissionRPC.java
+++ iris-client-bff/src/main/java/iris/client_bff/events/eps/DataSubmissionRPC.java
@@ -3,13 +3,16 @@ package iris.client_bff.events.eps;
 import com.googlecode.jsonrpc4j.JsonRpcParam;
 import com.googlecode.jsonrpc4j.JsonRpcService;
 import iris.client_bff.events.web.dto.GuestList;
+
+import javax.validation.Valid;
 import java.util.UUID;
 
 @JsonRpcService("/data-submission-rpc")
 public interface DataSubmissionRPC {
 
-  String submitGuestList(
-      @JsonRpcParam(value = "requestId") UUID providerId,
-      @JsonRpcParam(value = "guestList") GuestList guestList);
+    String submitGuestList(
+            @Valid @JsonRpcParam(value = "_client") JsonRpcClientDto client,
+            @JsonRpcParam(value = "requestId") UUID providerId,
+            @JsonRpcParam(value = "guestList") GuestList guestList);
 
 }
