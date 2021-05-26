7d95c9b24d6e6c919dfc27cf967e266f908142bf
Andreas Finke <hey-johnnypark@users.noreply.github.com>
2021-05-26 15:13:25 +0100

    fix: Fixes errors in release process and updates documentation based on user feedback.
    * Fixes error that hidden file .env.sample was not included in the final zip.
    * Updates errors in documentation.
    * Adds more information regarding ordering certificates from IRIS Team.
    * Adds explanation to proxy params.
    * Update docs for CSR Prozess.
    * Fixes wrong folder path for nginx conf
    
    PR: #107

---
+++ infrastructure/deployment/scripts/create-csr-for-proxy.sh
@@ -0,0 +1,16 @@
+ST=$1
+L=$2
+CN=$3
+
+O="Gesundheitsamt"
+ST=$1
+L=$2
+C="DE"
+OU="IT"
+CN=$3
+LEN="4096"
+
+openssl genrsa -out "${CN}.key" ${LEN};
+openssl rsa -in "${CN}.key" -pubout -out "${CN}.pub";
+
+openssl req -new -sha256 -key "${CN}.key" -subj "/C=${C}/ST=${ST}/L=${L}/O=${O}/OU=${OU}/CN=${CN}" -addext "keyUsage=digitalSignature" -addext "subjectAltName = URI:iris-name://${CN},URI:iris-group://private-proxies,DNS:${CN},DNS:*.${CN}.local"  -out "${CN}.csr";
