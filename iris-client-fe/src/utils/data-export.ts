c49089ba23b37d1ea58f58635e9f382232892927
Sebastian Wegener <sebastian.wegener@lionmint.com>
2021-05-28 15:44:07 +0200

    feat: returned changes that were not neccessary. Checked working of the export.
    Refs: iris-gateway/iris-backlog#74

--- iris-client-fe/src/utils/data-export.ts
+++ iris-client-fe/src/utils/data-export.ts
@@ -1,5 +1,3 @@
-import { Guest } from "@/api";
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { Parser } = require("json2csv");
 
@@ -20,7 +18,6 @@ export type TableRow = {
   phone: string;
   mobilePhone: string;
   address: string;
-  raw: Guest;
 };
 
 export type ContactCaseData = {
@@ -202,13 +199,10 @@ const exportAlternativeStandardCsv = function (
 };
 
 const exportSormasEventParticipantsCsv = function (
-  rows: Array<TableRow>,
+  rows: Array<EventParticipantData>,
   fileName: string
 ): Promise<string> {
   const headers = headerSormasEventParticipants;
-  const separator = ";";
-
-  const data = convertTableRowToEventParticipationData(rows, separator);
 
   return new Promise((resolve, reject) => {
     const fields = headers
@@ -221,6 +215,7 @@ const exportSormasEventParticipantsCsv = function (
       })
       .filter((v) => v);
     try {
+      const separator = ";";
       const parser = new Parser({
         fields,
         withBOM: true,
@@ -228,7 +223,7 @@ const exportSormasEventParticipantsCsv = function (
         delimiter: separator,
         quote: "",
       });
-      const csv = parser.parse(data);
+      const csv = parser.parse(sanitiseRows(rows, separator));
       downloadCsvFile(fileName, csv);
       resolve(csv);
     } catch (error) {
@@ -333,55 +328,6 @@ export const sanitiseField = function (
     }
     return field;
   } else return "";
-};
-
-const convertTableRowToEventParticipationData = function (
-  tableRows: TableRow[],
-  separator: string
-): EventParticipantData[] {
-  const data: EventParticipantData[] = [];
-
-  const headerInstance: EventParticipantData = {
-    involvementDescription: "involvementDescription",
-    firstName: "person.firstName",
-    lastName: "person.lastName",
-    sex: "person.sex",
-    phone: "person.phone",
-    email: "person.emailAddress",
-    postalCode: "person.address.postalCode",
-    city: "person.address.city",
-    street: "person.address.street",
-    houseNumber: "person.address.houseNumber",
-  };
-  data.push(headerInstance);
-
-  tableRows.forEach((element) => {
-    const involvementDescription =
-      sanitiseField(element.comment, separator) +
-      " // " +
-      sanitiseField(element.checkInTime, separator) +
-      " Uhr bis " +
-      sanitiseField(element.checkOutTime, separator) +
-      " Uhr (Maximale Kontaktdauer " +
-      sanitiseField(element.maxDuration, separator) +
-      ")";
-
-    const dataInstance: EventParticipantData = {
-      involvementDescription: involvementDescription,
-      firstName: sanitiseField(element.firstName, separator),
-      lastName: sanitiseField(element.lastName, separator),
-      sex: sanitiseField(element.raw.sex, separator) || "",
-      phone: sanitiseField(element.phone, separator),
-      email: sanitiseField(element.email, separator),
-      postalCode: sanitiseField(element.raw.address?.zipCode, separator) || "",
-      city: sanitiseField(element.raw.address?.city, separator) || "",
-      street: sanitiseField(element.raw.address?.street, separator) || "",
-      houseNumber:
-        sanitiseField(element.raw.address?.houseNumber, separator) || "",
-    };
-    data.push(dataInstance);
-  });
-  return data;
 };
 
 const dataExport = {
