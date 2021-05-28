31aeafac5bd0179f821582d54bb94271b143c17c
Fabio Aversente <fabio.aversente@blueboot-consulting.de>
2021-05-28 18:19:00 +0200

    chore: some minor code improvements

--- iris-client-bff/src/main/java/iris/client_bff/auth/db/CustomLogoutHandler.java
+++ iris-client-bff/src/main/java/iris/client_bff/auth/db/CustomLogoutHandler.java
@@ -3,10 +3,12 @@ package iris.client_bff.auth.db;
 import static iris.client_bff.auth.db.SecurityConstants.BEARER_TOKEN_PREFIX;
 
 import iris.client_bff.auth.db.jwt.JWTService;
+import lombok.AllArgsConstructor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -15,20 +17,17 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 @Service
+@AllArgsConstructor
 public class CustomLogoutHandler implements LogoutHandler {
 
 	private final JWTService jwtService;
-
-	public CustomLogoutHandler(JWTService jwtService) {
-		this.jwtService = jwtService;
-	}
 
 	@Override
 	public void logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
 			Authentication authentication) {
 		String header = httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION);
 
-		if (header == null || !header.startsWith(BEARER_TOKEN_PREFIX)) {
+		if (StringUtils.isEmpty(header) || !header.startsWith(BEARER_TOKEN_PREFIX)) {
 			return;
 		}
 		var token = header.replace(BEARER_TOKEN_PREFIX, "");
--- iris-client-bff/src/main/java/iris/client_bff/auth/db/jwt/JWTWhitelistCleanup.java
+++ iris-client-bff/src/main/java/iris/client_bff/auth/db/jwt/JWTWhitelistCleanup.java
@@ -1,5 +1,7 @@
 package iris.client_bff.auth.db.jwt;
 
+import lombok.AllArgsConstructor;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -8,14 +10,11 @@ import org.springframework.stereotype.Service;
 @Service
 @Configuration
 @EnableScheduling
+@AllArgsConstructor
 public class JWTWhitelistCleanup {
 
 	private final JWTService jwtService;
 	private final long DELETION_RATE = 30*60*1000; // 30 minutes
-
-	public JWTWhitelistCleanup(JWTService jwtService) {
-		this.jwtService = jwtService;
-	}
 
 	@Scheduled(fixedDelay = DELETION_RATE)
 	public void clean() {
