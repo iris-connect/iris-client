64fdf7950a4a67e9ebeb7ba22284c0f6e37e0969
Till Jannik Plewe <t.plewe@ostfalia.de>
2021-06-03 16:14:20 +0200

    feedbackbutton class

--- iris-client-fe/src/components/feedback-button.component.vue
+++ iris-client-fe/src/components/feedback-button.component.vue
@@ -1,19 +1,24 @@
-
-<template>
-  <v-btn
-    class="mr-0 mb-0"
-    large
-    fab
-    dark
-    fixed
-    bottom
-    right
-    :max-height="48"
-    :max-width="48"
-    @click="showFeedbackDialog = true"
-  >
-    <v-icon :size="30"> mdi-chat-alert-outline </v-icon>
-  </v-btn>
+<template v-slot:activator="{ on }">
+        <v-btn
+          class="mr-0 mb-0"
+          large
+          fab
+          dark
+          fixed
+          bottom
+          right
+          :max-height="48"
+          :max-width="48"
+          v-on="on"
+        >
+          <v-icon :size="30"> mdi-chat-alert-outline </v-icon>
+        </v-btn>
 </template>
+
+  <script lang="ts">
+  import Vue from 'vue'
+  export default class FeedbackButton extends Vue {
+  }
+  </script>
 
 <!-- TODO Make the feedback button its own component and import the component in the main views (home etc)>
