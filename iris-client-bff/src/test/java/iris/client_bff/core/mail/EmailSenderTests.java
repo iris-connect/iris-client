9a528a6a31ab01e7506ba4a71488a74ca8af0f68
Jens Kutzsche <github@gebea.de>
2021-06-08 09:17:09 +0200

    chore: implements tests for mail dispatch and extends config test
    Refs iris-connect/iris-backlog#85

---
+++ iris-client-bff/src/test/java/iris/client_bff/core/mail/EmailSenderTests.java
@@ -0,0 +1,163 @@
+package iris.client_bff.core.mail;
+
+import static org.assertj.core.api.Assertions.*;
+
+import io.vavr.control.Try;
+import iris.client_bff.IrisWebIntegrationTest;
+import iris.client_bff.core.EmailAddress;
+import iris.client_bff.core.mail.EmailSender.AbstractTemplatedEmail;
+import iris.client_bff.core.mail.EmailSender.AbstractTemplatedEmail.Recipient;
+import iris.client_bff.core.mail.EmailTemplates.Key;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Locale;
+import java.util.Map;
+
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.icegreen.greenmail.util.GreenMailUtil;
+
+@RequiredArgsConstructor
+@IrisWebIntegrationTest
+@ActiveProfiles("dev")
+class EmailSenderTests {
+
+	final EmailSender sender;
+	final MessageSourceAccessor messages;
+	final TestEmailServer mailServer;
+
+	private TestEmails testEmails;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		mailServer.reset();
+
+		testEmails = new TestEmails(sender, messages);
+	}
+
+	@Test
+	void sendSimpleMailCorrect_fixSenderAndRecipient_En() throws MessagingException {
+
+		testEmails.sendTestEmailEn();
+
+		mailServer.assertEmailSent(message -> {
+
+			assertThat(message.getSubject()).isEqualTo("This is a test mail");
+			assertThat(GreenMailUtil.getBody(message)).startsWith("Hi Name");
+			assertThat(message.getRecipients(RecipientType.TO)[0])
+					.hasToString("\"Test Person - test {at} test.de\" <testmailbox@iris-gateway.de>");
+			assertThat(message.getFrom()[0]).hasToString("Gesundheitsamt Entenhausen <mail-ga@entenhausen.de>");
+		});
+	}
+
+	@Test
+	void sendSimpleMailCorrect_fixSenderAndRecipient_De() throws MessagingException {
+
+		testEmails.sendTestEmailDe();
+
+		mailServer.assertEmailSent(message -> {
+
+			assertThat(message.getContentType()).contains("text/plain");
+			assertThat(message.getSubject()).isEqualTo("Das ist eine Test-Mail");
+			assertThat(GreenMailUtil.getBody(message)).startsWith("Hallo Name");
+			assertThat(message.getRecipients(RecipientType.TO)[0])
+					.hasToString("\"Test Person - test {at} test.de\" <testmailbox@iris-gateway.de>");
+			assertThat(message.getFrom()[0]).hasToString("Gesundheitsamt Entenhausen <mail-ga@entenhausen.de>");
+		});
+	}
+
+	@Test
+	void sendHtmlMailCorrect_fixSenderAndRecipient_De() throws MessagingException {
+
+		testEmails.sendTestHtmlEmailDe();
+
+		mailServer.assertEmailSent(message -> {
+
+			assertThat(message.getContentType()).contains("multipart/mixed");
+			assertThat(message.getSubject()).isEqualTo("Das ist eine Test-Mail");
+			assertThat(GreenMailUtil.getBody(message)).contains("Hallo Name");
+			assertThat(message.getRecipients(RecipientType.TO)[0])
+					.hasToString("\"Test Person - test {at} test.de\" <testmailbox@iris-gateway.de>");
+			assertThat(message.getFrom()[0]).hasToString("Gesundheitsamt Entenhausen <mail-ga@entenhausen.de>");
+		});
+	}
+
+	public class TestEmails extends EmailProvider {
+
+		public TestEmails(EmailSender emailSender, MessageSourceAccessor messages) {
+			super(emailSender, messages);
+		}
+
+		Try<Void> sendTestEmailEn() {
+
+			var english = Locale.ENGLISH;
+			var subject = messages.getMessage("TestMail.subject", english);
+
+			var email = new TestEmail(subject, TestKeys.TEST_MAIL_FTL, getParameters(), english);
+
+			return sendMail(email);
+		}
+
+		Try<Void> sendTestEmailDe() {
+
+			var locale = Locale.GERMAN;
+			var subject = messages.getMessage("TestMail.subject", locale);
+
+			var email = new TestEmail(subject, TestKeys.TEST_MAIL_FTL, getParameters(), locale);
+
+			return sendMail(email);
+		}
+
+		Try<Void> sendTestHtmlEmailDe() {
+
+			var locale = Locale.GERMAN;
+			var subject = messages.getMessage("TestMail.subject", locale);
+
+			var email = new TestEmail(subject, TestKeys.TEST_HTML_MAIL_FTLH, getParameters(), locale);
+
+			return sendMail(email);
+		}
+
+		private Map<String, String> getParameters() {
+			var parameters = Map.of("recipientName", "Name", "text", "Der Text", "senderName", "Sender");
+			return parameters;
+		}
+	}
+
+	class TestEmail extends AbstractTemplatedEmail {
+
+		TestEmail(String subject, EmailTemplates.Key template, Map<String, ? extends Object> placeholders, Locale locale) {
+
+			super(new TestRecipient(), subject, template, placeholders, locale);
+		}
+	}
+
+	enum TestKeys implements Key {
+
+		TEST_MAIL_FTL, TEST_HTML_MAIL_FTLH;
+
+		@Override
+		public String getKey() {
+			return name().toLowerCase(Locale.US).replace("_ftl", ".ftl").replace('_', '-');
+		}
+	}
+
+	class TestRecipient implements Recipient {
+
+		@Override
+		public String getFullName() {
+			return "Test Person";
+		}
+
+		@Override
+		public EmailAddress getEmailAddress() {
+			return EmailAddress.of("test@test.de");
+		}
+	}
+}
