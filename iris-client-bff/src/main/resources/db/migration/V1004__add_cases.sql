5710b01c9159ee3bb99a7878d9bd2630dfe5f059
Fabio Aversente <fabio.aversente@blueboot-consulting.de>
2021-06-21 08:55:22 +0200

    feat: added contact and address table creation

--- iris-client-bff/src/main/resources/db/migration/V1004__add_cases.sql
+++ iris-client-bff/src/main/resources/db/migration/V1004__add_cases.sql
@@ -13,3 +13,36 @@ CREATE TABLE case_data_submission (
 	FOREIGN KEY (request_id) REFERENCES case_data_request(request_id)
 );
 
+CREATE TABLE  contact (
+    contact_id uuid NOT NULL,
+    submission_id uuid NOT NULL,
+    first_name varchar(100) NULL,
+    last_name varchar(100) NULL,
+    date_of_birth date NULL,
+    sex varchar(20) NULL,
+    email varchar(100) NULL,
+    phone varchar(100) NULL,
+    mobile_phone varchar(100) NULL,
+    address_id uuid NULL,
+    workplace_address_id uuid NULL,
+    workplace_name varchar(256) NULL,
+    workplace_point_of_contact varchar(256) NULL,
+    first_contact_date date NULL,
+    last_contact_date date NULL,
+    contact_category varchar(100) NULL,
+    basic_conditions varchar(256) NULL,
+    PRIMARY KEY (contact_id),
+    FOREIGN KEY (submission_id) REFERENCES case_data_submission(submission_id),
+    FOREIGN KEY (address_id) REFERENCES address(address_id),
+    FOREIGN KEY (workplace_address_id) REFERENCES address(address_id)
+);
+
+CREATE TABLE address (
+    address_id uuid NOT NULL,
+    street varchar(256) NULL,
+    house_number varchar(256) NULL,
+    zip_code varchar(10) NULL,
+    city varchar(256) NULL,
+    PRIMARY KEY (id)
+);
+
