f7cfce26dfadad317b6bdff4e3ac0012594e3a1c
Fabio Aversente <fabio.aversente@blueboot-consulting.de>
2021-06-21 12:16:17 +0200

    feat: added case_event table

--- iris-client-bff/src/main/resources/db/migration/V1004__add_cases.sql
+++ iris-client-bff/src/main/resources/db/migration/V1004__add_cases.sql
@@ -40,3 +40,17 @@ CREATE TABLE  contact (
     PRIMARY KEY (contact_id),
     FOREIGN KEY (submission_id) REFERENCES case_data_submission(submission_id)
 );
+
+CREATE TABLE case_event (
+    event_id uuid NOT NULL,
+    submission_id uuid NOT NULL,
+    name varchar(256) NULL,
+    phone varchar(100) NULL,
+    street varchar(256) NULL,
+    house_number varchar(256) NULL,
+    zip_code varchar(10) NULL,
+    city varchar(256) NULL,
+    additional_information varchar(256),
+    PRIMARY KEY (event_id),
+    FOREIGN KEY (submission_id) REFERENCES case_data_submission(submission_id)
+);
