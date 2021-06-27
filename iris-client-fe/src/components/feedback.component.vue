73d6ccc4e3b78463f1a1293f630a30e4ceed2462
Edo4th <marius.rosteck@gmail.com>
2021-06-27 18:53:50 +0200

    added finishedDialog

--- iris-client-fe/src/components/feedback.component.vue
+++ iris-client-fe/src/components/feedback.component.vue
@@ -3,22 +3,22 @@
     <v-dialog v-model="show" max-width="40%">
       <template v-slot:activator="{ on, attrs }">
         <v-btn
-          v-bind="{ ...$attrs, ...attrs }"
-          class="mr-0 mb-0"
-          large
-          fab
-          dark
-          fixed
-          bottom
-          right
-          elevation="10"
-          :max-height="48"
-          :max-width="48"
-          v-on="on"
-          @click="form.model.name = userDisplayName()"
+            v-bind="{ ...$attrs, ...attrs }"
+            class="mr-0 mb-0"
+            large
+            fab
+            dark
+            fixed
+            bottom
+            right
+            elevation="10"
+            :max-height="48"
+            :max-width="48"
+            v-on="on"
+            @click="form.model.name = userDisplayName()"
         >
-          <v-icon :size="30"> mdi-chat-alert-outline </v-icon>
-          <slot />
+          <v-icon :size="30"> mdi-chat-alert-outline</v-icon>
+          <slot/>
         </v-btn>
       </template>
       <v-card style="height: fit-content">
@@ -31,80 +31,80 @@
             <v-card-subtitle style="text-align: center">
               <span class="subtitle-1">Wir freuen uns über Ihr Feedback.</span>
             </v-card-subtitle>
-            <v-divider class="theme--light primary" />
+            <v-divider class="theme--light primary"/>
           </div>
           <v-container class="mt-1 px-sm-15">
             <v-autocomplete
-              v-model="form.model.category"
-              :items="['Verbesserungsvorschlag', 'Problem']"
-              label="Kategorie auswählen*"
-              required
-              :rules="kategoryRules"
+                v-model="form.model.category"
+                :items="['Verbesserungsvorschlag', 'Problem']"
+                label="Kategorie auswählen*"
+                required
+                :rules="kategoryRules"
             ></v-autocomplete>
 
             <v-text-field
-              v-model="form.model.title"
-              label="Titel*"
-              required
-              :rules="titelRules"
-              maxlength="100"
+                v-model="form.model.title"
+                label="Titel*"
+                required
+                :rules="titelRules"
+                maxlength="100"
             ></v-text-field>
 
             <v-textarea
-              v-model="form.model.comment"
-              class="justify-center"
-              label="Ihr Platz für Feedback*"
-              no-resize
-              outlined
-              counter
-              required
-              :rules="textRules"
-              maxlength="1000"
+                v-model="form.model.comment"
+                class="justify-center"
+                label="Ihr Platz für Feedback*"
+                no-resize
+                outlined
+                counter
+                required
+                :rules="textRules"
+                maxlength="1000"
             ></v-textarea>
 
             <v-text-field
-              v-model="form.model.name"
-              class="justify-center"
-              label="Name"
-              maxlength="100"
+                v-model="form.model.name"
+                class="justify-center"
+                label="Name"
+                maxlength="100"
             ></v-text-field>
 
             <v-text-field
-              v-model="form.model.organisation"
-              class="justify-center"
-              label="Organisation"
-              required
-              maxlength="50"
+                v-model="form.model.organisation"
+                class="justify-center"
+                label="Organisation"
+                required
+                maxlength="50"
             ></v-text-field>
 
             <v-text-field
-              v-model="form.model.email"
-              class="justify-center"
-              label="E-Mail"
-              :rules="emailPatternRules"
-              required
-              maxlength="80"
+                v-model="form.model.email"
+                class="justify-center"
+                label="E-Mail"
+                :rules="emailPatternRules"
+                required
+                maxlength="80"
             ></v-text-field>
           </v-container>
-          <v-divider class="theme--light primary" />
+          <v-divider class="theme--light primary"/>
 
           <v-container>
             <v-row>
               <v-btn
-                class="ma-4"
-                color="secondary"
-                plain
-                @click="showCancelDialog = true"
+                  class="ma-4"
+                  color="secondary"
+                  plain
+                  @click="showCancelDialog = true"
               >
                 Abbrechen
               </v-btn>
               <v-spacer></v-spacer>
 
               <v-btn
-                class="ma-4"
-                color="primary"
-                :disabled="!isFormValid"
-                @click="showConfirmDialog = true"
+                  class="ma-4"
+                  color="primary"
+                  :disabled="!isFormValid"
+                  @click="showConfirmDialog = true"
               >
                 Absenden
               </v-btn>
@@ -115,26 +115,26 @@
     </v-dialog>
 
     <v-dialog
-      style="overflow-y: hidden"
-      v-model="showCancelDialog"
-      max-width="20%"
-      persistent
+        style="overflow-y: hidden"
+        v-model="showCancelDialog"
+        max-width="20%"
+        persistent
     >
       <v-card style="height: fit-content">
         <v-card-title class="justify-center">
           <span>ACHTUNG!</span>
         </v-card-title>
-        <v-divider class="theme--light primary" />
+        <v-divider class="theme--light primary"/>
         <v-card-text class="mt-3">
           Wenn Sie auf "Bestätigen" klicken, schließen Sie den Feedback-Bogen.
         </v-card-text>
         <v-card-actions>
           <v-btn
-            class="mt-4"
-            color="secondary"
-            plain
-            text
-            @click="showCancelDialog = false"
+              class="mt-4"
+              color="secondary"
+              plain
+              text
+              @click="showCancelDialog = false"
           >
             Zurück
           </v-btn>
@@ -147,28 +147,52 @@
     </v-dialog>
 
     <v-dialog
-      style="overflow-y: hidden"
-      v-model="showConfirmDialog"
-      max-width="20%"
-      persistent
+        v-model="showFinishedDialog"
+        max-width="20%"
+    >
+      <v-card style="height: fit-content">
+        <v-card-title class="justify-center">
+          <span>Danke!</span>
+        </v-card-title>
+        <v-divider class="theme--light primary"/>
+        <v-card-text class="mt-3" style="text-align: center">
+          Ihr Feedback wurde erfolgreich versendet.
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+              color="primary"
+              @click="showFinishedDialog = false"
+          >
+            OK
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog
+        style="overflow-y: hidden"
+        v-model="showConfirmDialog"
+        max-width="20%"
+        persistent
     >
       <v-card style="height: fit-content">
         <v-card-title class="justify-center">
           <span>Bestätigen</span>
         </v-card-title>
-        <v-divider class="theme--light primary" />
+        <v-divider class="theme--light primary"/>
         <v-card-text class="mt-3">
           Wenn Sie Ihr Feedback abschicken möchten, dann klicken Sie auf
           "Absenden".
         </v-card-text>
         <v-card-actions class="justify-center">
           <v-btn
-            class="mt-4"
-            color="secondary"
-            plain
-            text
-            :disabled="!isFormValid"
-            @click="showConfirmDialog = false"
+              class="mt-4"
+              color="secondary"
+              plain
+              text
+              :disabled="!isFormValid"
+              @click="showConfirmDialog = false"
           >
             Zurück
           </v-btn>
@@ -183,13 +207,13 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import {Component, Vue} from "vue-property-decorator";
 import {
   Configuration,
   IrisClientFrontendApiFactory,
   FeedbackInsert,
 } from "@/api";
-import axios, { AxiosResponse } from "axios";
+import axios, {AxiosResponse} from "axios";
 import config from "@/config";
 
 type FeedbackForm = {
@@ -211,26 +235,28 @@ export default class FeedbackDialog extends Vue {
   show = false;
   showCancelDialog = false;
   showConfirmDialog = false;
+  showFinishedDialog = false;
   isFormValid = false;
   kategoryRules = [(content: any) => !!content || "Pflichtfeld"];
   titelRules = [
     (content: any) => !!content || "Pflichtfeld",
     (content: string | any[]) =>
-      (content && content.length <= 100) ||
-      "Der Titel darf nicht mehr als 100 Zeichen beinhalten.",
+        (content && content.length <= 100) ||
+        "Der Titel darf nicht mehr als 100 Zeichen beinhalten.",
   ];
   textRules = [
     (content: any) => !!content || "Pflichtfeld",
     (content: string | any[]) =>
-      (content && content.length <= 1000) ||
-      "Das Feedback darf nicht mehr als 1000 Zeichen beinhalten.",
+        (content && content.length <= 1000) ||
+        "Das Feedback darf nicht mehr als 1000 Zeichen beinhalten.",
   ];
   emailPatternRules = [
     (content: string) =>
-      !content ||
-      /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(content) ||
-      "E-mail muss gültig oder leer sein.",
+        !content ||
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(content) ||
+        "E-mail muss gültig oder leer sein.",
   ];
+
   cancel() {
     this.showCancelDialog = false;
     this.show = false;
@@ -242,21 +268,22 @@ export default class FeedbackDialog extends Vue {
 
   confirm() {
     this.showConfirmDialog = false;
+    this.showFinishedDialog = true;
     this.show = false;
     this.sendStuff();
   }
 
   sendStuff() {
     const authAxiosInstance = axios.create();
-    const { apiBaseURL } = config;
+    const {apiBaseURL} = config;
     const clientConfig = new Configuration({
       basePath: apiBaseURL,
     });
 
     const feedbackClient = IrisClientFrontendApiFactory(
-      clientConfig,
-      undefined,
-      authAxiosInstance
+        clientConfig,
+        undefined,
+        authAxiosInstance
     );
     try {
       feedbackClient.feedbackPost(this.form.model);
