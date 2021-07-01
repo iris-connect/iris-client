6dc7764018512309a3c5210b3f27f4a674e0bc57
Benedikt Schneppe <benedikt.schneppe@check24.de>
2021-07-01 14:53:44 +0200

    fix: restrict cross origin POST to relevant cross origins

--- iris-client-bff/src/main/java/iris/client_bff/config/CORSConfig.java
+++ iris-client-bff/src/main/java/iris/client_bff/config/CORSConfig.java
@@ -2,6 +2,7 @@ package iris.client_bff.config;
 
 import static iris.client_bff.config.DataSubmissionConfig.DATA_SUBMISSION_ENDPOINT;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -12,13 +13,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CORSConfig {
 
   @Bean
-  public WebMvcConfigurer configureCorsForJsonRPC() {
+  public WebMvcConfigurer configureCorsForJsonRPC(@Value("#{${ext.corsPatterns}}") final String[] allowedOriginPatterns) {
     return new WebMvcConfigurer() {
       @Override
       public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping(DATA_SUBMISSION_ENDPOINT)
-            .allowedOrigins("*")
+            .allowedOriginPatterns(allowedOriginPatterns)
             .allowedMethods("OPTIONS", "POST")
             .allowedHeaders("*");
       }
