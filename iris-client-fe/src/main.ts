5ede69d038989a0e5351301c5231c01595ef8753
Nuts <mad-nuts@users.noreply.github.com>
2021-07-23 12:56:20 +0200

    Feature/196 security headers (#221)
    for testing purposes in staging configuration
    * feat: adds webpack compression plugin for gzipping files
    * feat: adds security headers to nginx staging config + sets irisAppContext as node attribute + JSON parsing to avoid Content Security Policy script-src: unsafe-inline
    * feat: adds caching to nginx staging config

--- iris-client-fe/src/main.ts
+++ iris-client-fe/src/main.ts
@@ -3,11 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import initAppContext from "@/utils/appContext";
 
 import "@fontsource/roboto/latin.css";
 import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
+
+// in case: config.ts file is loaded before main.ts
+initAppContext();
 
 new Vue({
   router,
