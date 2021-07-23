6492e3a690537ee92b137d2f28c2c40eb1f26623
Nuts <mad-nuts@users.noreply.github.com>
2021-07-23 12:55:11 +0200

    feat: use local fonts for gdpr compliance (#220)
    chore: use local fonts for gdpr compliance (#220)

--- iris-client-fe/src/main.ts
+++ iris-client-fe/src/main.ts
@@ -4,6 +4,9 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 
+import "@fontsource/roboto/latin.css";
+import "@mdi/font/css/materialdesignicons.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
