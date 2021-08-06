82171324476e31034dbaa6d8286ecb5cfd0d6645
Jens Kutzsche <github@gebea.de>
2021-08-06 12:10:06 +0200

    chore: adds value checks and descriptions for properties
    Refs iris-connect/iris-backlog#185

--- iris-client-bff/src/main/java/iris/client_bff/auth/db/login_attempts/LoginAttemptsProperties.java
+++ iris-client-bff/src/main/java/iris/client_bff/auth/db/login_attempts/LoginAttemptsProperties.java
@@ -5,8 +5,11 @@ import lombok.Data;
 
 import java.time.Duration;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.util.Assert;
 
 /**
  * @author Jens Kutzsche
@@ -22,4 +25,14 @@ public class LoginAttemptsProperties {
 	private Duration firstWaitingTime;
 	private int waitingTimeMultiplier;
 	private Duration ignoreOldAttemptsAfter;
+
+	@PostConstruct
+	void checkValues() {
+		Assert.isTrue(firstWarningThreshold > 0, "firstWarningThreshold must be > 0!");
+		Assert.isTrue(warningThresholdMultiplier > 0, "warningThresholdMultiplier must be > 0!");
+		Assert.isTrue(!firstWaitingTime.isNegative() && !firstWaitingTime.isZero(), "firstWaitingTime must be > 0!");
+		Assert.isTrue(waitingTimeMultiplier > 0, "waitingTimeMultiplier must be > 0!");
+		Assert.isTrue(!ignoreOldAttemptsAfter.isNegative() && !ignoreOldAttemptsAfter.isZero(),
+				"ignoreOldAttemptsAfter must be > 0!");
+	}
 }
