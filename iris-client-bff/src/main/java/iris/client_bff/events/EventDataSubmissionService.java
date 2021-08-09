de018f32c56446949f6702bff189ff1a6130d8ee
Timo Boegner <tboegner@googlemail.com>
2021-08-09 13:49:44 +0200

    feat: Simplify abort announcement logic. Reject data submissions when data request status is not "REQUESTED". Refs: iris-connect/iris-backlog#218

--- iris-client-bff/src/main/java/iris/client_bff/events/EventDataSubmissionService.java
+++ iris-client-bff/src/main/java/iris/client_bff/events/EventDataSubmissionService.java
@@ -1,28 +1,85 @@
 package iris.client_bff.events;
 
+import iris.client_bff.core.log.LogHelper;
 import iris.client_bff.events.EventDataRequest.Status;
 import iris.client_bff.events.model.EventDataSubmission;
 import iris.client_bff.events.model.Guest;
 import iris.client_bff.events.model.GuestListDataProvider;
 import iris.client_bff.events.web.dto.GuestList;
+import iris.client_bff.proxy.IRISAnnouncementException;
+import iris.client_bff.proxy.ProxyServiceClient;
 import lombok.AllArgsConstructor;
 
+import java.util.UUID;
 import java.util.stream.Collectors;
 
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @AllArgsConstructor
 public class EventDataSubmissionService {
+
+	private static final String ERR_SUBM_NOT_ALLOWED = "Error: Submission not allowed for case ";
 
 	private final ModelMapper mapper;
 
 	private final EventDataSubmissionRepository submissions;
 
 	private final EventDataRequestService requestService;
+	private final EventEmailProvider eventEmailProvider;
+	private final ProxyServiceClient proxyClient;
 
-	public void save(EventDataRequest dataRequest, GuestList guestList) {
+	public String findRequestAndSaveGuestList(UUID dataAuthorizationToken, GuestList guestList) {
+		return requestService.findById(dataAuthorizationToken).map(dataRequest -> {
+
+			switch (dataRequest.getStatus()) {
+				case ABORTED: {
+					logSubmissionStatus("aborted", dataAuthorizationToken, dataRequest.getId());
+					return ERR_SUBM_NOT_ALLOWED + dataAuthorizationToken.toString() + ". Request was aborted.";
+				}
+				case CLOSED: {
+					logSubmissionStatus("closed", dataAuthorizationToken, dataRequest.getId());
+					return ERR_SUBM_NOT_ALLOWED + dataAuthorizationToken.toString() + ". Request already closed.";
+				}
+				case DATA_RECEIVED: {
+					logSubmissionStatus("received", dataAuthorizationToken, dataRequest.getId());
+					return ERR_SUBM_NOT_ALLOWED + dataAuthorizationToken.toString() + ". Data already received.";
+				}
+				case DATA_REQUESTED:
+					logSubmissionStatus("requested", dataAuthorizationToken, dataRequest.getId());
+					break;
+				default: {
+					logSubmissionStatus(dataRequest.getStatus().toString(), dataAuthorizationToken, dataRequest.getId());
+					return ERR_SUBM_NOT_ALLOWED + dataAuthorizationToken.toString() + ". Data not requested.";
+				}
+			}
+
+			save(dataRequest, guestList);
+			eventEmailProvider.sendDataReceivedEmailAsynchronously(dataRequest);
+
+			try {
+				proxyClient.abortAnnouncement(dataRequest.getAnnouncementToken());
+			} catch (IRISAnnouncementException e) {
+				log.error("Abort announcement failed for {}", LogHelper.obfuscateAtStart8(dataRequest.getAnnouncementToken()), e);
+			}
+			return "OK";
+
+		}).orElseGet(() -> {
+			log.error("Data submission for unknown data request occurred: {}", LogHelper.obfuscateAtStart8(dataAuthorizationToken.toString()));
+
+			return "Unknown dataAuthorizationToken: " + dataAuthorizationToken;
+		});
+	}
+
+	private void logSubmissionStatus(String status, UUID dataAuthorizationToken, EventDataRequest.DataRequestIdentifier dataRequestId) {
+		log.trace("Submission {} for {} event {}", LogHelper.obfuscateAtStart8(dataAuthorizationToken.toString()),
+				status,	LogHelper.obfuscateAtStart8(dataRequestId.toString()));
+	}
+
+	private void save(EventDataRequest dataRequest, GuestList guestList) {
 		var guests = guestList.getGuests().stream().map(it -> mapper.map(it, Guest.class)).collect(Collectors.toSet());
 
 		var dataProvider = mapper.map(guestList.getDataProvider(), GuestListDataProvider.class);
@@ -44,8 +101,4 @@ public class EventDataSubmissionService {
 		requestService.save(dataRequest);
 	}
 
-	public void deleteFailedSubmissionAttempt(EventDataRequest dataRequest, GuestList guestList) {
-		submissions.findAllByRequest(dataRequest).get().findFirst().ifPresent(it -> submissions.delete(it));
-		requestService.save(dataRequest);
-	}
 }
--- iris-client-bff/src/main/java/iris/client_bff/events/eps/EventDataControllerImpl.java
+++ iris-client-bff/src/main/java/iris/client_bff/events/eps/EventDataControllerImpl.java
@@ -1,12 +1,7 @@
 package iris.client_bff.events.eps;
 
-import iris.client_bff.events.EventDataRequest;
-import iris.client_bff.events.EventDataRequestService;
 import iris.client_bff.events.EventDataSubmissionService;
-import iris.client_bff.events.EventEmailProvider;
 import iris.client_bff.events.web.dto.GuestList;
-import iris.client_bff.proxy.IRISAnnouncementException;
-import iris.client_bff.proxy.ProxyServiceClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -19,47 +14,12 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EventDataControllerImpl implements EventDataController {
 
-	private final EventDataRequestService requestService;
 	private final EventDataSubmissionService dataSubmissionService;
-	private final EventEmailProvider eventEmailProvider;
-	private final ProxyServiceClient proxyClient;
-	private final DataProviderClient epsDataRequestClient;
 
 	@Override
 	public String submitGuestList(JsonRpcClientDto client, UUID dataAuthorizationToken, GuestList guestList) {
 		log.trace("Start submission {}", dataAuthorizationToken);
 
-		// Todo check client.getName() vs. providerId
-
-		return requestService.findById(dataAuthorizationToken).map(dataRequest -> {
-
-			if (dataRequest.getStatus().equals(EventDataRequest.Status.ABORTED)) {
-				log.trace("Submission {} for aborted event {}", dataAuthorizationToken, dataRequest.getId());
-				return "Error: Submission not allowed for " + dataAuthorizationToken.toString() + ". Request was aborted.";
-			}
-
-			try {
-				dataSubmissionService.save(dataRequest, guestList);
-				proxyClient.abortAnnouncement(dataRequest.getAnnouncementToken());
-				eventEmailProvider.sendDataReceivedEmailAsynchronously(dataRequest);
-
-			log.trace("Submission of data for {} is complete and proxy ancouncement has been closed", dataAuthorizationToken);
-			} catch (IRISAnnouncementException e) {
-				// Todo: Do I also need to remove the submission ? Possibly remove the saved data again
-				dataSubmissionService.deleteFailedSubmissionAttempt(dataRequest, guestList);
-				e.printStackTrace();
-				log.trace("Submission of data or closing of the proxy announcement failed for {}", dataRequest.getAnnouncementToken());
-			}
-			return "OK";
-
-		}).orElseGet(() -> {
-
-			// TODO sufficient?
-			// probably throw exception
-
-			log.error("Data submission for unknown data request occurred: {}", dataAuthorizationToken);
-
-			return "Unknown dataAuthorizationToken: " + dataAuthorizationToken.toString();
-		});
+		return dataSubmissionService.findRequestAndSaveGuestList(dataAuthorizationToken, guestList);
 	}
 }
