77a2ed20095344fe91bda14f9e3f0101cd9d07cb
Jens Kutzsche <github@gebea.de>
2021-08-12 19:17:17 +0200

    fix: Removes default admin credentials for production environment and prevents the application from starting if these properties are missing.
    Adapts code of InitialAdminLoader and his test to the new requirements.
    
    Improves other propertie validation too.
    
    Refs iris-connect/iris-backlog#223

--- iris-client-bff/src/main/java/iris/client_bff/auth/db/DbAuthProperties.java
+++ iris-client-bff/src/main/java/iris/client_bff/auth/db/DbAuthProperties.java
@@ -3,9 +3,12 @@ package iris.client_bff.auth.db;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "security.auth.db")
 @ConstructorBinding
@@ -14,9 +17,14 @@ import org.springframework.boot.context.properties.ConstructorBinding;
 @ConditionalOnProperty(
 		value = "security.auth",
 		havingValue = "db")
+@Validated
 public class DbAuthProperties {
 
+	@NotEmpty(
+			message = "The admin user name must be configured! (Environment variable: SECURITY_AUTH_DB_ADMIN_USER_NAME)")
 	private String adminUserName;
 
+	@NotEmpty(
+			message = "The admin user password must be configured! (Environment variable: SECURITY_AUTH_DB_ADMIN_USER_PASSWORD)")
 	private String adminUserPassword;
 }
