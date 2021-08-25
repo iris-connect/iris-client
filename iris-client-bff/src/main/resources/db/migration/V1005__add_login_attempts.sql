a5e7691e2ff42655aaf2a832b9de98e1d62267c3
Jens Kutzsche <github@gebea.de>
2021-08-25 11:30:21 +0200

    chore: fixes corrupt flyway script (#275)

--- iris-client-bff/src/main/resources/db/migration/V1005__add_login_attempts.sql
+++ iris-client-bff/src/main/resources/db/migration/V1005__add_login_attempts.sql
@@ -1,9 +1,9 @@
 CREATE TABLE login_attempts (
-	reference varchar(32) NOT NULL,
+	remote_addr varchar(32) NOT NULL,
 	attempts int NOT NULL,
 	next_warning_threshold int NOT NULL,
 	waiting_time bigint NOT NULL,
 	created timestamp NOT NULL,
 	last_modified timestamp NOT NULL,
-	PRIMARY KEY (reference)
+	PRIMARY KEY (remote_addr)
 );
