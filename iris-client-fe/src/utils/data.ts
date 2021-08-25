56489be6719d1af5bd8f09837ac3107a4c2b093f
andreas.kausler <aka@cosynus.de>
2021-08-25 15:00:32 +0200

    feat: normalize event tracking details in frontend (with diff notification)

--- iris-client-fe/src/utils/data.ts
+++ iris-client-fe/src/utils/data.ts
@@ -1,13 +1,27 @@
 import _get from "lodash/get";
+import dayjs from "./date";
+import _transform from "lodash/transform";
+import _isEqual from "lodash/isEqual";
+import _isObject from "lodash/isObject";
+import _isEmpty from "lodash/isEmpty";
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const entryNormalizer = <T>(obj?: T) => <K extends keyof T>(
+  key: K,
+  fallback: T[K],
+  type = "string"
+) => {
+  return normalize<T, K>(obj, key, type, fallback);
+};
 
 export const normalize = <T, K extends keyof T>(
-  obj: T | unknown,
+  obj: T | unknown | undefined,
   key: K,
   type: string,
   fallback: T[K]
 ): T[K] => {
   const val: T[K] = _get(obj, key);
-  if (val && validateType(val, type)) return val;
+  if (val !== undefined && validateType(val, type)) return val;
   return fallback;
 };
 
@@ -15,5 +29,41 @@ const validateType = (value: unknown, type: string): boolean => {
   if (type === "array") {
     return Array.isArray(value);
   }
+  if (type === "dateString") {
+    return typeof value === "string" && dayjs(value).isValid();
+  }
   return typeof value === type;
+};
+
+export const parseData = <T>(data: T): Partial<T> => {
+  try {
+    return JSON.parse(JSON.stringify(data));
+  } catch {
+    return data;
+  }
+};
+
+export const notifyDifference = <A>(a: A, b: A, msg?: string): void => {
+  if (a) {
+    const diffA = difference(a, b);
+    const diffB = difference(b, a);
+    if (!_isEmpty(diffA) || !_isEmpty(diffB)) {
+      console.log(msg, "diff A - B: ", diffA, "diff B - A: ", diffB);
+    }
+  }
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const difference = <A extends Record<string, any>, B extends A>(
+  object: A,
+  base: B
+): Record<string, unknown> => {
+  return _transform(object, (result, value, key) => {
+    if (!_isEqual(value, base[key])) {
+      result[key] =
+        _isObject(value) && _isObject(base[key])
+          ? difference(value, base[key])
+          : [value, base[key]];
+    }
+  });
 };
--- iris-client-fe/src/views/event-tracking-details/event-tracking-details.store.ts
+++ iris-client-fe/src/views/event-tracking-details/event-tracking-details.store.ts
@@ -1,9 +1,23 @@
-import { DataRequestClientUpdate, DataRequestDetails } from "@/api";
+import {
+  Address,
+  DataRequestClientUpdate,
+  DataRequestDetails,
+  Guest,
+  GuestAllOfAttendanceInformation,
+  GuestList,
+  GuestListDataProvider,
+  LocationAddress,
+  LocationContact,
+  LocationContext,
+  LocationInformation,
+} from "@/api";
 import { RootState } from "@/store/types";
 
 import { Commit, Dispatch, Module } from "vuex";
 import authClient from "@/api-client";
 import { ErrorMessage, getErrorMessage } from "@/utils/axios";
+import { entryNormalizer, notifyDifference, parseData } from "@/utils/data";
+import _isNil from "lodash/isNil";
 
 export type EventTrackingDetailsState = {
   eventTrackingDetails: DataRequestDetails | null;
@@ -92,8 +106,9 @@ const eventTrackingDetails: EventTrackingDetailsModule = {
       commit("setEventTrackingDetailsLoading", true);
       commit("setEventTrackingDetailsLoadingError", null);
       try {
-        eventTrackingDetails = (await authClient.getLocationDetails(eventId))
-          .data;
+        eventTrackingDetails = normalizeDataRequestDetails(
+          (await authClient.getLocationDetails(eventId)).data
+        );
       } catch (e) {
         commit("setEventTrackingDetailsLoadingError", getErrorMessage(e));
       } finally {
@@ -118,6 +133,150 @@ const eventTrackingDetails: EventTrackingDetailsModule = {
       await dispatch("fetchEventTrackingDetails", payload.id);
     },
   },
+};
+
+const normalizeLocationAddress = (
+  locationAddress?: LocationAddress
+): LocationAddress => {
+  const normalizer = entryNormalizer(locationAddress);
+  return {
+    street: normalizer("street", ""),
+    city: normalizer("city", ""),
+    zip: normalizer("zip", ""),
+  };
+};
+
+const normalizeLocationContact = (
+  locationContact?: LocationContact
+): LocationContact => {
+  const normalizer = entryNormalizer(locationContact);
+  return {
+    officialName: normalizer("officialName", undefined),
+    representative: normalizer("representative", undefined),
+    address: normalizeLocationAddress(locationContact?.address),
+    ownerEmail: normalizer("ownerEmail", undefined),
+    email: normalizer("email", undefined),
+    phone: normalizer("phone", undefined),
+  };
+};
+
+const normalizeLocationContext = (
+  locationContext?: LocationContext
+): LocationContext => {
+  const normalizer = entryNormalizer(locationContext);
+  return {
+    id: normalizer("id", ""),
+    name: normalizer("name", ""),
+  };
+};
+
+const normalizeLocationInformation = (
+  locationInformation?: LocationInformation
+): LocationInformation => {
+  const normalizer = entryNormalizer(locationInformation);
+  const contexts = normalizer("contexts", undefined, "array");
+  return {
+    id: normalizer("id", ""),
+    providerId: normalizer("providerId", ""),
+    name: normalizer("name", ""),
+    publicKey: normalizer("publicKey", undefined),
+    contact: normalizeLocationContact(locationInformation?.contact),
+    contexts: contexts?.map(normalizeLocationContext),
+  };
+};
+
+const normalizeAddress = (address?: Address): Address => {
+  const normalizer = entryNormalizer(address);
+  return {
+    street: normalizer("street", undefined),
+    houseNumber: normalizer("houseNumber", undefined),
+    zipCode: normalizer("zipCode", undefined),
+    city: normalizer("city", undefined),
+  };
+};
+
+const normalizeGuestAllOfAttendanceInformation = (
+  attendanceInformation?: GuestAllOfAttendanceInformation
+): GuestAllOfAttendanceInformation => {
+  const normalizer = entryNormalizer(attendanceInformation);
+  return {
+    descriptionOfParticipation: normalizer(
+      "descriptionOfParticipation",
+      undefined
+    ),
+    attendFrom: normalizer("attendFrom", ""),
+    attendTo: normalizer("attendTo", ""),
+    additionalInformation: normalizer("additionalInformation", undefined),
+  };
+};
+
+const normalizeGuest = (guest?: Guest): Guest => {
+  const normalizer = entryNormalizer(guest);
+  return {
+    firstName: normalizer("firstName", ""),
+    lastName: normalizer("lastName", ""),
+    dateOfBirth: normalizer("dateOfBirth", undefined),
+    sex: normalizer("sex", undefined),
+    email: normalizer("email", undefined),
+    phone: normalizer("phone", undefined),
+    mobilePhone: normalizer("mobilePhone", undefined),
+    address: !_isNil(guest?.address)
+      ? normalizeAddress(guest?.address)
+      : guest?.address,
+    attendanceInformation: normalizeGuestAllOfAttendanceInformation(
+      guest?.attendanceInformation
+    ),
+    identityChecked: normalizer("identityChecked", undefined, "boolean"),
+  };
+};
+
+const normalizeGuestListDataProvider = (
+  dataProvider?: GuestListDataProvider
+): GuestListDataProvider => {
+  const normalizer = entryNormalizer(dataProvider);
+  return {
+    name: normalizer("name", ""),
+    address: normalizeAddress(dataProvider?.address),
+  };
+};
+
+const normalizeGuestList = (guestList?: GuestList): GuestList => {
+  const normalizer = entryNormalizer(guestList);
+  const guests = normalizer("guests", [], "array");
+  return {
+    guests: guests.map(normalizeGuest),
+    dataProvider: normalizeGuestListDataProvider(guestList?.dataProvider),
+    additionalInformation: normalizer("additionalInformation", undefined),
+    startDate: normalizer("startDate", undefined, "dateString"),
+    endDate: normalizer("endDate", undefined, "dateString"),
+  };
+};
+
+export const normalizeDataRequestDetails = (
+  details?: DataRequestDetails
+): DataRequestDetails => {
+  const normalizer = entryNormalizer(details);
+  const normalizedDetails = {
+    comment: normalizer("comment", undefined),
+    status: normalizer("status", undefined),
+    code: normalizer("code", undefined),
+    name: normalizer("name", undefined),
+    externalRequestId: normalizer("externalRequestId", undefined),
+    start: normalizer("start", undefined, "dateString"),
+    end: normalizer("end", undefined, "dateString"),
+    requestedAt: normalizer("requestedAt", undefined, "dateString"),
+    lastModifiedAt: normalizer("lastModifiedAt", undefined, "dateString"),
+    requestDetails: normalizer("requestDetails", undefined),
+    locationInformation: details?.locationInformation
+      ? normalizeLocationInformation(details?.locationInformation)
+      : details?.locationInformation,
+    submissionData: details?.submissionData
+      ? normalizeGuestList(details?.submissionData)
+      : undefined,
+  };
+  const parsedData = parseData(normalizedDetails);
+  notifyDifference(details, parsedData, "DataRequestDetails");
+  return parsedData;
 };
 
 export default eventTrackingDetails;
