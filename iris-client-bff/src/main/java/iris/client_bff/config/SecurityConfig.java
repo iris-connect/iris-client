f81ccdee4a8fe6632c6238f368c8db60b959ca4f
Jens Kutzsche <github@gebea.de>
2021-09-16 21:59:54 +0200

    chore: removes unnecessary dependency to bouncycastle

--- iris-client-bff/src/main/java/iris/client_bff/config/SecurityConfig.java
+++ iris-client-bff/src/main/java/iris/client_bff/config/SecurityConfig.java
@@ -1,22 +1,11 @@
 package iris.client_bff.config;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import javax.annotation.PostConstruct;
-import java.security.Security;
-
 @Configuration
 public class SecurityConfig {
-
-	@PostConstruct
-	public void initBouncyCastle() {
-
-		Security.addProvider(new BouncyCastleProvider());
-		Security.setProperty("crypto.policy", "unlimited");
-	}
 
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
