bec3158b22bcf17c2732086c1e6b1fb9ba9361dd
andreas.kausler <aka@cosynus.de>
2021-10-15 12:41:47 +0200

    fix: fix e2e test github action

--- iris-client-fe/tests/unit/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import HomeView from "@/views/home/home.vue";
-
-describe("HelloWorld.vue", () => {
-  it("can be mounted", () => {
-    const wrapper = shallowMount(HomeView);
-    expect(wrapper.exists()).toBe(true);
-  });
-});
