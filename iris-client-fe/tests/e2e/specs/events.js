83e90294e96b1e6479ece6c6fb365bc824a67885
andreas.kausler <aka@cosynus.de>
2021-10-18 11:27:22 +0200

    feat: add test for event creation form validation

--- iris-client-fe/tests/e2e/specs/events.js
+++ iris-client-fe/tests/e2e/specs/events.js
@@ -1,4 +1,4 @@
-// import dayjs from "dayjs";
+import dayjs from "dayjs";
 
 describe("Events", () => {
   beforeEach(() => {
@@ -67,59 +67,83 @@ describe("Events", () => {
       });
     cy.getBy("location-select.dialog").should("not.be.visible");
   });
-  // it("should validate and auto-fill the event creation form", () => {
-  //   cy.visit("/events/new");
-  //   cy.location("pathname").should("equal", "/events/new");
-  //   cy.get("form")
-  //     .should("exist")
-  //     .within(() => {
-  //       cy.getBy("button{submit}").click();
-  //       cy.assertInputInvalid("input{externalId}", "Pflichtfeld");
-  //       cy.assertInputValid("input{name}");
-  //       cy.assertInputInvalid(
-  //         "location-select.dialog.activator",
-  //         "Bitte wählen Sie einen Ereignisort aus"
-  //       );
-  //       cy.getBy("start")
-  //         .assertInputInvalid(
-  //           "Bitte geben Sie einen Zeitpunkt in der Vergangenheit an"
-  //         )
-  //         .within(() => {
-  //           cy.assertInputInvalid(
-  //             "date-input-field",
-  //             "Bitte geben Sie ein Datum an"
-  //           )
-  //             .type("1234")
-  //             .assertInputInvalid(
-  //               "Bitte geben Sie ein Datum im Format DD.MM.YYYY ein"
-  //             )
-  //             .clear()
-  //             .type(dayjs().subtract(1, "day").format("DD.MM.YYYY"))
-  //             .assertInputValid();
-  //           cy.getBy("time-input-field")
-  //             .should("have.value", dayjs().format("HH:mm"))
-  //             .assertInputValid()
-  //             .clear()
-  //             .assertInputInvalid(
-  //               "time-input-field",
-  //               "Bitte geben Sie eine Uhrzeit an"
-  //             )
-  //             .type("1234")
-  //             .assertInputInvalid(
-  //               "Bitte geben Sie eine Uhrzeit im Format HH:mm an"
-  //             );
-  //         });
-  //       cy.getBy("end").within(() => {
-  //         cy.assertInputInvalid(
-  //           "date-input-field",
-  //           "Bitte geben Sie ein Datum an"
-  //         );
-  //         cy.assertInputInvalid(
-  //           "time-input-field",
-  //           "Bitte geben Sie eine Uhrzeit an"
-  //         );
-  //       });
-  //       cy.assertInputValid("textarea{requestDetails}");
-  //     });
-  // });
+  it("should validate and auto-fill the event creation form", () => {
+    cy.visit("/events/new");
+    cy.location("pathname").should("equal", "/events/new");
+    cy.get("form")
+      .should("exist")
+      .within(() => {
+        cy.getBy("button{submit}").click();
+        cy.assertInputInvalid("input{externalId}", "Pflichtfeld");
+        cy.assertInputValid("input{name}");
+        cy.assertInputValid("textarea{requestDetails}");
+        cy.assertInputInvalid(
+          "location-select.dialog.activator",
+          "Bitte wählen Sie einen Ereignisort aus"
+        );
+        cy.getBy("start")
+          .assertInputInvalid(
+            "Bitte geben Sie einen Zeitpunkt in der Vergangenheit an"
+          )
+          .within(() => {
+            cy.getBy("date-input-field")
+              .assertInputInvalid("Bitte geben Sie ein Datum an")
+              .type("1234")
+              .assertInputInvalid(
+                "Bitte geben Sie ein Datum im Format DD.MM.YYYY ein"
+              )
+              .clear()
+              .type(dayjs().subtract(1, "day").format("DD.MM.YYYY"))
+              .assertInputValid();
+            cy.getBy("time-input-field")
+              .assertInputInvalid("Bitte geben Sie eine Uhrzeit an")
+              .type("1234")
+              .assertInputInvalid(
+                "Bitte geben Sie eine Uhrzeit im Format HH:mm an"
+              )
+              .clear()
+              .type(dayjs().format("HH:mm"))
+              .assertInputValid();
+          });
+        cy.getBy("start")
+          .assertInputValid()
+          .within(() => {
+            cy.getBy("date-input-field")
+              .clear()
+              .type(dayjs().add(1, "day").format("DD.MM.YYYY"));
+          });
+        cy.getBy("start")
+          .assertInputInvalid(
+            "Bitte geben Sie einen Zeitpunkt in der Vergangenheit an"
+          )
+          .within(() => {
+            cy.getBy("date-input-field")
+              .clear()
+              .type(dayjs().subtract(1, "day").format("DD.MM.YYYY"));
+          });
+        cy.getBy("end").within(() => {
+          cy.getBy("date-input-field").should(
+            "have.value",
+            dayjs().subtract(1, "day").format("DD.MM.YYYY")
+          );
+          cy.getBy("time-input-field")
+            .should("have.value", "23:59")
+            .assertInputValid()
+            .clear()
+            .assertInputInvalid("Bitte geben Sie eine Uhrzeit an")
+            .type("1234")
+            .assertInputInvalid(
+              "Bitte geben Sie eine Uhrzeit im Format HH:mm an"
+            )
+            .clear()
+            .type("23:59");
+          cy.getBy("date-input-field")
+            .clear()
+            .type(dayjs().subtract(2, "day").format("DD.MM.YYYY"));
+        });
+        cy.getBy("end").assertInputInvalid(
+          "Bitte geben Sie einen Zeitpunkt an, der nach dem Beginn liegt"
+        );
+      });
+  });
 });
