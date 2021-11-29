d2392bc94de62fe889631e8ea4dcf9d7dc58a4d0
Jens Kutzsche <github@gebea.de>
2021-11-29 17:24:37 +0100

    chore: fixes foreign key constraint violation on `DELETE FROM case_data_request`

--- iris-client-bff/src/main/resources/db/migration/V1007__readable_token_for_cases.sql
+++ iris-client-bff/src/main/resources/db/migration/V1007__readable_token_for_cases.sql
@@ -1,3 +1,6 @@
+DELETE FROM case_event;
+DELETE FROM contact;
+DELETE FROM case_data_submission;
 DELETE FROM case_data_request;
 
 ALTER TABLE case_data_request ADD data_authorization_token VARCHAR(255) NOT NULL;
