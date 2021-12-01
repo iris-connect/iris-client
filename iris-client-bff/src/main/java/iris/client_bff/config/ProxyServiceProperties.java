92f44bba301d0cd00998cfde43df03e24932af32
Jens Kutzsche <github@gebea.de>
2021-12-01 12:26:29 +0100

    chore: fixes details of the central configuration resulting from the tests with the dev and test environments
    * The proxy subdomain is now that of the dev environment.
    * Uses the fixed error code to ignore missing configurations at startup.
    * Insert more documentation into the code.
    * The `proxy-service.target-subdomain` is required now and hasn't a default anymore.
    * `hd.abbreviation` was unnecessary

--- iris-client-bff/src/main/java/iris/client_bff/config/ProxyServiceProperties.java
+++ iris-client-bff/src/main/java/iris/client_bff/config/ProxyServiceProperties.java
@@ -2,6 +2,8 @@ package iris.client_bff.config;
 
 import lombok.Value;
 
+import javax.validation.constraints.NotBlank;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
@@ -11,7 +13,10 @@ import org.springframework.validation.annotation.Validated;
 @Value
 @Validated
 public class ProxyServiceProperties {
+
 	private final String epsName;
 	private final String targetProxy;
+
+	@NotBlank
 	private final String targetSubdomain;
 }
