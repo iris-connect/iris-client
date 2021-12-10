af73f61e5c2e6fd0376a33a153f6786843d6dedd
Jens Kutzsche <github@gebea.de>
2021-12-10 13:13:38 +0100

    fix: Improves timeouts in Client and extends the checks for the status (EPS version) to better determine the status.
    * Makes previously suppressed internal exceptions visible again.
    * Improves exception handling and message resolving.
    * Fixes an NPE with old EPS versions.
    * Fixes regex for timeouts.
    * Adds a check of the app EPS version.
    * Adds more status messages.
    * Adds logging for internal and unknown errors.
    
    PR #509

--- iris-client-bff/src/main/java/iris/client_bff/status/AppInfo.java
+++ iris-client-bff/src/main/java/iris/client_bff/status/AppInfo.java
@@ -3,6 +3,8 @@ package iris.client_bff.status;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @author Jens Kutzsche
  */
@@ -13,4 +15,8 @@ public class AppInfo {
 	private final String name;
 	private final String version;
 	private AppStatus status;
+
+	boolean isTooOld() {
+		return StringUtils.isBlank(version);
+	}
 }
--- iris-client-bff/src/main/java/iris/client_bff/status/eps/EPSStatusClient.java
+++ iris-client-bff/src/main/java/iris/client_bff/status/eps/EPSStatusClient.java
@@ -5,20 +5,25 @@ import static java.util.stream.Collectors.*;
 import iris.client_bff.config.RPCClientConfig;
 import iris.client_bff.status.AppInfo;
 import iris.client_bff.status.AppStatusException;
+import iris.client_bff.status.AppStatusInternalException;
 import iris.client_bff.status.Apps;
 import iris.client_bff.status.Apps.App;
+import iris.client_bff.status.EpsConnectionException;
 import lombok.AllArgsConstructor;
 
 import java.net.ConnectException;
+import java.net.SocketTimeoutException;
 import java.util.List;
 import java.util.Set;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.googlecode.jsonrpc4j.JsonRpcClientException;
 import com.googlecode.jsonrpc4j.JsonRpcHttpClient;
 
 @Service
@@ -27,6 +32,7 @@ public class EPSStatusClient {
 
 	private final JsonRpcHttpClient epsRpcClient;
 	private final RPCClientConfig rpcClientConfig;
+	private final MessageSourceAccessor messages;
 
 	public Apps getAvailableApps() {
 
@@ -40,8 +46,10 @@ public class EPSStatusClient {
 					.map(App::new)
 					.collect(collectingAndThen(toList(), Apps::new));
 
+		} catch (JsonRpcClientException e) {
+			throw new AppStatusException(e.getMessage());
 		} catch (Throwable t) {
-			throw new AppStatusException(t.getMessage(), false);
+			throw new AppStatusInternalException(t.getMessage(), t);
 		}
 	}
 
@@ -53,12 +61,16 @@ public class EPSStatusClient {
 
 			var ping = epsRpcClient.invoke(methodName, null, Ping.class);
 
-			return new AppInfo(ping.serverInfo.name, ping.version);
+			var name = ping.serverInfo != null ? ping.serverInfo.name : messages.getMessage("StatusService.unknown_app_name");
 
-		} catch (ConnectException e) {
-			throw new AppStatusException(e.getMessage(), true);
+			return new AppInfo(name, ping.version);
+
+		} catch (ConnectException | SocketTimeoutException e) { // Connection timeout
+			throw new EpsConnectionException(e.getMessage());
+		} catch (JsonRpcClientException e) {
+			throw new AppStatusException(e.getMessage());
 		} catch (Throwable t) {
-			throw new AppStatusException(t.getMessage(), false);
+			throw new AppStatusInternalException(t.getMessage(), t);
 		}
 	}
 
