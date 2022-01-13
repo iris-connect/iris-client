fc23fc0c8c8af77c85dba0640642c64e883fc6bf
lucky-lusa <tim@lusa.de>
2022-01-13 12:14:42 +0100

    feature: eps certificate is validated, removes getAllCertsTrustedSSLContext from JSON-RPC client

--- iris-client-bff/src/main/java/iris/client_bff/config/RPCClientConfig.java
+++ iris-client-bff/src/main/java/iris/client_bff/config/RPCClientConfig.java
@@ -86,30 +86,6 @@ public class RPCClientConfig {
 		return client;
 	}
 
-	private SSLContext getAllCertsTrustedSSLContext() throws NoSuchAlgorithmException, KeyManagementException {
-		TrustManager[] trustAllCerts = new TrustManager[] {
-				new X509TrustManager() {
-					@Override
-					public X509Certificate[] getAcceptedIssuers() {
-						return null;
-					}
-
-					@Override
-					public void checkClientTrusted(
-							X509Certificate[] certs, String authType) {}
-
-					@Override
-					public void checkServerTrusted(
-							X509Certificate[] certs, String authType) {}
-				}
-		};
-
-		SSLContext sc = SSLContext.getInstance("SSL");
-		sc.init(null, trustAllCerts, new SecureRandom());
-		HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-		return sc;
-	}
-
 	/**
 	 * @author Jens Kutzsche
 	 */
