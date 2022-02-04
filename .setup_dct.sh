20f4381115bb24d5f0ecc550c9ba66bb1c3f5b86
Tim <62595633+lucky-lusa@users.noreply.github.com>
2022-02-04 08:08:50 +0100

    feat: Images are signed before uploading to docker to insure their integrity.
    Adds dct to all release images.
    
    + Makes the image tag of BFF image explicit and reduces the Risk of errors due to inconsistencies.
    
    Co-authored-by: Jens Kutzsche <github@gebea.de>
    PR #569

---
+++ .setup_dct.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# exit when any command fails
+set -e
+
+DCT_PATH=~/.docker/trust/private/
+
+DCT_KEY_FILE="$DCT_PATH$DCT_PRIVATE_KEY_IDENTIFIER.key"
+
+mkdir -p "$DCT_PATH"
+
+echo $DCT_PRIVATE_KEY > $DCT_KEY_FILE
+
+chmod 600 "$DCT_KEY_FILE"
+
+DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE="$DCT_PRIVATE_KEY_PASSPHRASE" DOCKER_CONTENT_TRUST=1 docker trust key load "$DCT_KEY_FILE"
+
