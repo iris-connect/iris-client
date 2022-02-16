c75d2c55c43b096b97d50194732c30c2d792102f
andreas.kausler <aka@cosynus.de>
2022-02-16 17:35:41 +0100

    fix: move iris-message-file table creation to a new migration file (V1010 is skipped because it is reserved for message data)

--- iris-client-bff/src/main/resources/db/migration_mssql/V1009__add_iris_messages.sql
+++ iris-client-bff/src/main/resources/db/migration_mssql/V1009__add_iris_messages.sql
@@ -36,18 +36,3 @@ CREATE TABLE iris_message (
     FOREIGN KEY (created_by) REFERENCES user_accounts(user_id),
     FOREIGN KEY (last_modified_by) REFERENCES user_accounts(user_id)
 );
-
-CREATE TABLE iris_message_file (
-    id binary(255) NOT NULL,
-    message_id binary(255) NOT NULL,
-    name varchar(255) NOT NULL,
-    content varbinary(max) NULL,
-    created datetime2 NOT NULL,
-    last_modified datetime2 NOT NULL,
-    created_by binary(255) NULL,
-    last_modified_by binary(255) NULL,
-    CONSTRAINT iris_message_file_pkey PRIMARY KEY (id),
-    CONSTRAINT iris_message_file_message_fk FOREIGN KEY (message_id) REFERENCES iris_message(id),
-    FOREIGN KEY (created_by) REFERENCES user_accounts(user_id),
-    FOREIGN KEY (last_modified_by) REFERENCES user_accounts(user_id)
-);
