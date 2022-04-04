60d0ad7fccf1bb4f7af6266a9c7d8df209f18c77
Jens Kutzsche <github@gebea.de>
2022-04-04 16:58:03 +0200

    chore: code cleanup

--- iris-client-bff/src/main/java/iris/client_bff/iris_messages/web/IrisMessageDataController.java
+++ iris-client-bff/src/main/java/iris/client_bff/iris_messages/web/IrisMessageDataController.java
@@ -1,18 +1,29 @@
 package iris.client_bff.iris_messages.web;
 
 import iris.client_bff.core.utils.ValidationHelper;
-import iris.client_bff.iris_messages.*;
 import iris.client_bff.iris_messages.IrisMessageData.IrisMessageDataIdentifier;
+import iris.client_bff.iris_messages.IrisMessageDataProcessors;
+import iris.client_bff.iris_messages.IrisMessageDataService;
 import iris.client_bff.iris_messages.exceptions.IrisMessageDataException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -32,23 +43,22 @@ public class IrisMessageDataController {
 	private final MessageSourceAccessor messages;
 
 	@PostMapping("/{messageDataId}/import")
-	public ResponseEntity<?> importMessageDataAndAdd(
+	@ResponseStatus(HttpStatus.OK)
+	public void importMessageDataAndAdd(
 			@PathVariable IrisMessageDataIdentifier messageDataId,
-			@RequestParam(required = false) UUID importTargetId,
-			@RequestBody(required = false) String importSelection
-	) {
-		if (importTargetId == null) {
-			return importMessageDataAndAdd(messageDataId);
-		}
-		return importMessageDataAndUpdate(messageDataId, importTargetId, importSelection);
+			@RequestParam Optional<UUID> importTargetId,
+			@RequestBody Optional<String> importSelection) {
+
+		importTargetId.ifPresentOrElse(
+				targetId -> importMessageDataAndUpdate(messageDataId, targetId, importSelection.get()),
+				() -> importMessageDataAndAdd(messageDataId));
 	}
 
-	private ResponseEntity<?> importMessageDataAndAdd(IrisMessageDataIdentifier messageDataId) {
+	private void importMessageDataAndAdd(IrisMessageDataIdentifier messageDataId) {
 		this.messageDataService.importMessageData(messageDataId);
-		return ResponseEntity.ok(null);
 	}
 
-	private ResponseEntity<?> importMessageDataAndUpdate(
+	private void importMessageDataAndUpdate(
 			IrisMessageDataIdentifier messageDataId,
 			UUID importTargetId,
 			String importSelection
@@ -61,7 +71,6 @@ public class IrisMessageDataController {
 			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
 		}
 		this.messageDataService.importMessageData(messageDataId, importTargetId, importSelection);
-		return ResponseEntity.ok(null);
 	}
 
 	@GetMapping("/{messageDataId}/import-selection-view")
