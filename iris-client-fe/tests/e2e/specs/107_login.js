0ef1cea7abe0c717d3ad1d3aca0c671282556bf7
andreas.kausler <aka@cosynus.de>
2022-04-08 08:15:02 +0200

    fix: fix dependabot security alert and update multiple npm dependencies

--- iris-client-fe/tests/e2e/specs/107_login.js
+++ iris-client-fe/tests/e2e/specs/107_login.js
@@ -1,6 +1,6 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-import {getTestAdminCredentials} from "../support/commands";
+import { getTestAdminCredentials } from "../support/commands";
 
 describe("Login", () => {
   beforeEach(() => {
